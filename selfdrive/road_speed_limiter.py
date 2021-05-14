b8e9ea9e12bce13fbd908838483ede47c156328c
DFyffe <fyffed@netzero.net>
2021-05-14 08:28:04 -0400

    curve tune 3

--- selfdrive/road_speed_limiter.py
+++ selfdrive/road_speed_limiter.py
@@ -121,14 +121,14 @@ class RoadSpeedLimiter:
 
         if cam_type == 7:
           if self.longcontrol:
-            sec = interp(diff_speed, [15., 19.], [15., 20.])
+            sec = interp(diff_speed, [15., 19.], [15., 18.])
           else:
-            sec = interp(diff_speed, [15., 19.], [16., 21.])
+            sec = interp(diff_speed, [15., 19.], [16., 19.])
         else:
           if self.longcontrol:
-            sec = interp(diff_speed, [15., 19.], [14., 19.])
+            sec = interp(diff_speed, [15., 19.], [14., 17.])
           else:
-            sec = interp(diff_speed, [15., 19.], [15., 21.])
+            sec = interp(diff_speed, [15., 19.], [15., 18.])
 
         if MIN_LIMIT <= cam_limit_speed <= MAX_LIMIT and (self.slowing_down or cam_limit_speed_left_dist < v_ego * sec):
 
