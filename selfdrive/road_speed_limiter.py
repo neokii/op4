290e5966545ddf19797a870e6b3305445332fc29
DFyffe <fyffed@netzero.net>
2021-05-14 21:23:42 -0400

    Update road_speed_limiter.py

--- selfdrive/road_speed_limiter.py
+++ selfdrive/road_speed_limiter.py
@@ -121,14 +121,14 @@ class RoadSpeedLimiter:
 
         if cam_type == 7:
           if self.longcontrol:
-            sec = interp(diff_speed, [15., 19.], [15., 18.])
+            sec = interp(diff_speed, [14., 40.], [15., 22.])
           else:
-            sec = interp(diff_speed, [15., 19.], [16., 19.])
+            sec = interp(diff_speed, [14., 40.], [16., 23.])
         else:
           if self.longcontrol:
-            sec = interp(diff_speed, [15., 19.], [14., 17.])
+            sec = interp(diff_speed, [14., 40.], [12., 18.])
           else:
-            sec = interp(diff_speed, [15., 19.], [15., 18.])
+            sec = interp(diff_speed, [14., 40.], [13., 20.])
 
         if MIN_LIMIT <= cam_limit_speed <= MAX_LIMIT and (self.slowing_down or cam_limit_speed_left_dist < v_ego * sec):
 
