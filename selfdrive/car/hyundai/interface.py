545f335325fbffa2519565614770798530a0ab69
DFyffe <fyffed@netzero.net>
2021-05-19 12:22:21 -0400

    5/19/21 update conflict resolved #3

--- selfdrive/car/hyundai/interface.py
+++ selfdrive/car/hyundai/interface.py
@@ -66,20 +66,20 @@ class CarInterface(CarInterfaceBase):
 
     # longitudinal
     ret.longitudinalTuning.kpBP = [0, 10. * CV.KPH_TO_MS, 20. * CV.KPH_TO_MS, 40. * CV.KPH_TO_MS, 70. * CV.KPH_TO_MS, 100. * CV.KPH_TO_MS]
-    ret.longitudinalTuning.kpV = [0.9, 0.75, 0.55, 0.42, 0.37, 0.33]
+    ret.longitudinalTuning.kpV = [0.95, 0.75, 0.57, 0.45, 0.4, 0.36]
     ret.longitudinalTuning.kiBP = [0.]
     ret.longitudinalTuning.kiV = [0.015]
-    ret.longitudinalTuning.kf = 0.7
+    ret.longitudinalTuning.kf = 0.9
     ret.longitudinalTuning.deadzoneBP = [0., 100. * CV.KPH_TO_MS]
     ret.longitudinalTuning.deadzoneV = [0., 0.015]
 
     ret.gasMaxBP = [0., 10. * CV.KPH_TO_MS, 20. * CV.KPH_TO_MS, 70. * CV.KPH_TO_MS, 130. * CV.KPH_TO_MS]
-    ret.gasMaxV = [0.42, 0.28, 0.20, 0.14, 0.11]
+    ret.gasMaxV = [0.43, 0.3, 0.22, 0.14, 0.11]
 
     ret.brakeMaxBP = [0.]
     ret.brakeMaxV = [1.35]
 
-    ret.stoppingBrakeRate = 0.1  # brake_travel/s while trying to stop
+    ret.stoppingBrakeRate = 0.15  # brake_travel/s while trying to stop
     ret.startingBrakeRate = 1.0  # brake_travel/s while releasing on restart
     ret.startAccel = 1.3
 
