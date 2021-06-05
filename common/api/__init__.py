64ff7dfea3031762b7b3a9577034eb3068c23de6
Circuit Pro Industries <oricalworks@gmail.com>
2021-06-05 16:13:09 -0400

    Update __init__.py

--- common/api/__init__.py
+++ common/api/__init__.py
@@ -34,7 +34,7 @@ class Api():
     
 
 def api_get(endpoint, method='GET', timeout=None, access_token=None, **params):
-  backend = "https://api.retropilot.org"
+  backend = "https://api.retropilot.org/g"
 
   headers = {}
   if access_token is not None:
