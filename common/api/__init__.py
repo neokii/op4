87672fee377cd3ccbb7975aeae0e50bbd49b085c
Circuit Pro Industries <oricalworks@gmail.com>
2021-06-05 16:06:17 -0400

    FIX  Retro Pilot API

--- common/api/__init__.py
+++ common/api/__init__.py
@@ -34,7 +34,7 @@ class Api():
     
 
 def api_get(endpoint, method='GET', timeout=None, access_token=None, **params):
-  backend = "https://api.commadotai.com/"
+  backend = "https://api.retropilot.org"
 
   headers = {}
   if access_token is not None:
