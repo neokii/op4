01ec255c278236f87b779d6bc9fc8c0029c7fa7d
Circuit Pro Industries <oricalworks@gmail.com>
2021-06-06 09:18:42 -0400

    fix uploads pt2

--- selfdrive/manager/process_config.py
+++ selfdrive/manager/process_config.py
@@ -6,13 +6,13 @@ from selfdrive.hardware import EON, TICI, PC
 WEBCAM = os.getenv("WEBCAM") is not None
 
 procs = [
-  # DaemonProcess("manage_athenad", "selfdrive.athena.manage_athenad", "AthenadPid"),
+  DaemonProcess("manage_athenad", "selfdrive.athena.manage_athenad", "AthenadPid"),
   # due to qualcomm kernel bugs SIGKILLing camerad sometimes causes page table corruption
   NativeProcess("camerad", "selfdrive/camerad", ["./camerad"], unkillable=True, driverview=True),
   NativeProcess("clocksd", "selfdrive/clocksd", ["./clocksd"]),
   NativeProcess("dmonitoringmodeld", "selfdrive/modeld", ["./dmonitoringmodeld"], enabled=(not PC or WEBCAM), driverview=True),
-  #NativeProcess("logcatd", "selfdrive/logcatd", ["./logcatd"]),
-  #NativeProcess("loggerd", "selfdrive/loggerd", ["./loggerd"]),
+  NativeProcess("logcatd", "selfdrive/logcatd", ["./logcatd"]),
+  NativeProcess("loggerd", "selfdrive/loggerd", ["./loggerd"]),
   NativeProcess("modeld", "selfdrive/modeld", ["./modeld"]),
   NativeProcess("proclogd", "selfdrive/proclogd", ["./proclogd"]),
   NativeProcess("sensord", "selfdrive/sensord", ["./sensord"], enabled=not PC, persistent=EON, sigkill=EON),
@@ -23,7 +23,7 @@ procs = [
   PythonProcess("controlsd", "selfdrive.controls.controlsd"),
   PythonProcess("deleter", "selfdrive.loggerd.deleter", persistent=True),
   PythonProcess("dmonitoringd", "selfdrive.monitoring.dmonitoringd", enabled=(not PC or WEBCAM), driverview=True),
-  #PythonProcess("logmessaged", "selfdrive.logmessaged", persistent=True),
+  PythonProcess("logmessaged", "selfdrive.logmessaged", persistent=True),
   PythonProcess("pandad", "selfdrive.pandad", persistent=True),
   PythonProcess("paramsd", "selfdrive.locationd.paramsd"),
   PythonProcess("plannerd", "selfdrive.controls.plannerd"),
@@ -31,9 +31,9 @@ procs = [
   PythonProcess("rtshield", "selfdrive.rtshield", enabled=EON),
   PythonProcess("thermald", "selfdrive.thermald.thermald", persistent=True),
   PythonProcess("timezoned", "selfdrive.timezoned", enabled=TICI, persistent=True),
-  #PythonProcess("tombstoned", "selfdrive.tombstoned", enabled=not PC, persistent=True),
-  #PythonProcess("updated", "selfdrive.updated", enabled=not PC, persistent=True),
-  #PythonProcess("uploader", "selfdrive.loggerd.uploader", persistent=True),
+  PythonProcess("tombstoned", "selfdrive.tombstoned", enabled=not PC, persistent=True),
+  PythonProcess("updated", "selfdrive.updated", enabled=not PC, persistent=True),
+  PythonProcess("uploader", "selfdrive.loggerd.uploader", persistent=True),
 ]
 
 managed_processes = {p.name: p for p in procs}
