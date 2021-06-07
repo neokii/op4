5ae673cbdcc2bf3f0fe09d5c76b6f5ee1688c5c7
Circuit Pro Industries <oricalworks@gmail.com>
2021-06-06 20:16:05 -0400

    Update params.cc

--- selfdrive/common/params.cc
+++ selfdrive/common/params.cc
@@ -230,7 +230,7 @@ std::unordered_map<std::string, uint32_t> keys = {
     {"ShowDebugUI", PERSISTENT},
     {"CustomLeadMark", PERSISTENT},
     {"UseSMDPSHarness", PERSISTENT},
-
+    {"hotspot_on_boot", PERSISTENT},
 };
 
 } // namespace
