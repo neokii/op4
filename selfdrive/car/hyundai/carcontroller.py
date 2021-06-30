7a7f43fdb249ab5ee1f2033dbf7b26bd90700c5c
Circuit Pro Industries <oricalworks@gmail.com>
2021-06-30 15:52:30 -0400

    Update carcontroller.py

--- selfdrive/car/hyundai/carcontroller.py
+++ selfdrive/car/hyundai/carcontroller.py
@@ -103,14 +103,13 @@ class CarController():
 
     # fix for Genesis hard fault at low speed
     if UseSMDPS == True:
-      if TypeMDPS == False:
-        min_set_speed = 0 * CV.KPH_TO_MS
-        if CS.out.vEgo < 55 * CV.KPH_TO_MS and self.car_fingerprint == CAR.GENESIS and not CS.mdps_bus:
-          lkas_active = False
-      else:
-        min_set_speed = 30 * CV.KPH_TO_MS
-        if v_ego < min_set_speed:
-          events.events.add(EventName.pedalPressed)
+      min_set_speed = 0 * CV.KPH_TO_MS
+      if CS.out.vEgo < 55 * CV.KPH_TO_MS and self.car_fingerprint == CAR.GENESIS and not CS.mdps_bus:
+        lkas_active = False
+    else:
+      min_set_speed = 30 * CV.KPH_TO_MS
+      if v_ego < min_set_speed:
+        events.events.add(EventName.pedalPressed)
 
 
 
