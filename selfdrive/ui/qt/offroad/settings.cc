c9bdd50ef9cc1a1acc127b6c21e42f3d208e6f71
Circuit Pro Industries <oricalworks@gmail.com>
2021-07-07 03:58:31 -0400

    Update settings.cc

--- selfdrive/ui/qt/offroad/settings.cc
+++ selfdrive/ui/qt/offroad/settings.cc
@@ -205,79 +205,79 @@ DevicePanel::DevicePanel(QWidget* parent) : QWidget(parent) {
 //Overide Boot Logo
 offroad_btns.append(new ButtonControl("Overide boot logo to Stinger.", "Stinger",
                                         "This changes the boot logo.", [=]() {
-    if (ConfirmationDialog::confirm("Change Boot logo to Kia Stinger?.", this))) {
+    if (ConfirmationDialog::confirm("Change Boot logo to Kia Stinger?.", this)) {
       //auto change boot logo Stinger
       int result;
       char oldname[] ="Stinger.png";
       char newname[] ="img_spinner_comma.png";
       result= rename( oldname , newname );
       if ( result == 0 )
-        puts ( "File successfully renamed" );
+        ConfirmationDialog::confirm("Success.", this
       else
-        perror( "Error renaming file" );
+        ConfirmationDialog::confirm("Failure.", this
       return 0;
     }
   }, "", this));
 offroad_btns.append(new ButtonControl("Overide boot logo to Hyundai.", "Hyundai",
                                         "This changes the boot logo.", [=]() {
-    if (ConfirmationDialog::confirm("Change Boot logo to Hyundai?.", this))) {
+    if (ConfirmationDialog::confirm("Change Boot logo to Hyundai?.", this)) {
       //auto change boot logo Hyundai
       int result;
       char oldname[] ="Hyundai.png";
       char newname[] ="img_spinner_comma.png";
       result= rename( oldname , newname );
       if ( result == 0 )
-        puts ( "File successfully renamed" );
+        ConfirmationDialog::confirm("Success.", this
       else
-        perror( "Error renaming file" );
+        ConfirmationDialog::confirm("Failure.", this
       return 0;
     }
   }, "", this));
 
 offroad_btns.append(new ButtonControl("Overide boot logo to Genesis.", "Genesis",
                                         "This changes the boot logo.", [=]() {
-    if (ConfirmationDialog::confirm("Change Boot logo to Kia Genesis?.", this))) {
+    if (ConfirmationDialog::confirm("Change Boot logo to Kia Genesis?.", this)) {
       //auto change boot logo Genesis
       int result;
       char oldname[] ="Stinger.png";
       char newname[] ="img_spinner_comma.png";
       result= rename( oldname , newname );
       if ( result == 0 )
-        puts ( "File successfully renamed" );
+        ConfirmationDialog::confirm("Success.", this
       else
-        perror( "Error renaming file" );
+        ConfirmationDialog::confirm("Failure.", this
       return 0;
     }
   }, "", this));
 
 offroad_btns.append(new ButtonControl("Overide boot logo to Kia.", "Kia",
                                         "This changes the boot logo.", [=]() {
-    if (ConfirmationDialog::confirm("Change Boot logo to Kia?.", this))) {
+    if (ConfirmationDialog::confirm("Change Boot logo to Kia?.", this)) {
       //auto change boot logo Kia
       int result;
       char oldname[] ="Kia.png";
       char newname[] ="img_spinner_comma.png";
       result= rename( oldname , newname );
       if ( result == 0 )
-        puts ( "File successfully renamed" );
+        ConfirmationDialog::confirm("Success.", this
       else
-        perror( "Error renaming file" );
+        ConfirmationDialog::confirm("Failure.", this
       return 0;
     }
   }, "", this));
 
 offroad_btns.append(new ButtonControl("Overide boot logo to Comma.", "Comma",
                                         "This changes the boot logo.", [=]() {
-    if (ConfirmationDialog::confirm("Change Boot logo to Comma?.", this))) {
+    if (ConfirmationDialog::confirm("Change Boot logo to Comma?.", this)) {
       //auto change boot logo Comma
       int result;
       char oldname[] ="Comma.png";
       char newname[] ="img_spinner_comma.png";
       result= rename( oldname , newname );
       if ( result == 0 )
-        puts ( "File successfully renamed" );
+        ConfirmationDialog::confirm("Success.", this
       else
-        perror( "Error renaming file" );
+        ConfirmationDialog::confirm("Failure.", this
       return 0;
     }
   }, "", this));
