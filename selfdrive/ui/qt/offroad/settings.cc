398b895b5a627352a837244a08c9e8fd2fa003f0
Circuit Pro Industries <oricalworks@gmail.com>
2021-07-08 19:41:12 -0400

    Update settings.cc

--- selfdrive/ui/qt/offroad/settings.cc
+++ selfdrive/ui/qt/offroad/settings.cc
@@ -474,7 +474,8 @@ QWidget * community_panel() {
                                             "",
                                             "../assets/offroad/icon_road.png"
                                             ));
-  toggles_list->addWidget(horizontal_line());  toggles_list->addWidget(new ParamControl("hotspot_on_boot",
+  toggles_list->addWidget(horizontal_line());
+  toggles_list->addWidget(new ParamControl("hotspot_on_boot",
                                             "Hot Spot On Start",
                                             "",
                                             "../assets/offroad/icon_road.png"
