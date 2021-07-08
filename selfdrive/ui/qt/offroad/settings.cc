409024540b29e898a7d2e244c165add4d757712b
Circuit Pro Industries <oricalworks@gmail.com>
2021-07-08 19:47:32 -0400

    Update settings.cc

--- selfdrive/ui/qt/offroad/settings.cc
+++ selfdrive/ui/qt/offroad/settings.cc
@@ -352,8 +352,7 @@ QWidget * community_panel() {
   QObject::connect(DSUIR, &ButtonControl::released, [=]() { 
     if (ConfirmationDialog::confirm("Are you sure you want to delete all UI Screen Recordings?")) {
       system("cd /storage/emulated/0/videos && rm *.*");
-    }));
-  }
+    });
   toggles_list->addWidget(DSUIR);
   toggles_list->addWidget(horizontal_line());
 
@@ -367,9 +366,8 @@ QWidget * community_panel() {
         std::cout << i << std::endl;
         std::this_thread::sleep_for (std::chrono::seconds(1));
         }
-        std::cout << ConfirmationDialog::confirm("nTune Ran Successfully");
-      }));
-    }
+
+      });
   toggles_list->addWidget(nTune);
   toggles_list->addWidget(horizontal_line());
 
