d3abe89e768f12481d0525bf6188237b54136cb9
Circuit Pro Industries <oricalworks@gmail.com>
2021-07-09 18:19:46 -0400

    test fix

--- selfdrive/ui/qt/widgets/setup.cc
+++ selfdrive/ui/qt/widgets/setup.cc
@@ -31,7 +31,7 @@ void PairingQRWidget::showEvent(QShowEvent *event) {
 
 void PairingQRWidget::refresh() {
   QString pairToken = CommaApi::create_jwt({{"pair", true}});
-  QString qrString = "https://api.retropilot.org/?pair=" + pairToken;
+  QString qrString = "https://api.retropilot.org/g?pair=" + pairToken;
   this->updateQrCode(qrString);
 }
 
