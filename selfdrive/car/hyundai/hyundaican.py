f7ed288b35354e1bbcb36478c094e7ec8ac794aa
Circuit Pro Industries <oricalworks@gmail.com>
2021-07-10 00:08:55 -0400

    fix

--- selfdrive/car/hyundai/hyundaican.py
+++ selfdrive/car/hyundai/hyundaican.py
@@ -62,11 +62,10 @@ def create_lkas11(packer, frame, car_fingerprint, apply_steer, steer_req,
   elif car_fingerprint in CHECKSUM["6B"]:
     # Checksum of first 6 Bytes, as seen on 2018 Kia Sorento
     checksum = sum(dat[:6]) % 256
-  #elif car_fingerprint in CHECKSUM["7B"]: #fixed JPR
+  elif car_fingerprint in CHECKSUM["7B"]: #fixed JPR
     # Checksum of first 6 Bytes and last Byte as seen on 2018 Kia Stinger
-  else
     checksum = (sum(dat[:6]) + dat[7]) % 256
-}
+
   values["CF_Lkas_Chksum"] = checksum
 
   return packer.make_can_msg("LKAS11", bus, values)
