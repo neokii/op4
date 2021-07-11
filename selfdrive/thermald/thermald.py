c7b1175ad2a38c8b4a79f396ebc25317a9cf7fc5
Circuit Pro Industries <oricalworks@gmail.com>
2021-07-11 14:39:02 -0400

    fix disable update

--- selfdrive/thermald/thermald.py
+++ selfdrive/thermald/thermald.py
@@ -363,6 +363,7 @@ def thermald_thread():
       set_offroad_alert_if_changed("Offroad_ConnectivityNeededPrompt", False)
 
     #startup_conditions["up_to_date"] = params.get("Offroad_ConnectivityNeeded") is None or params.get("DisableUpdates") == b"1"
+    startup_conditions["up_to_date"] = params.get("Offroad_ConnectivityNeeded") is None or params.get_bool("DisableUpdates")
     startup_conditions["not_uninstalling"] = not params.get_bool("DoUninstall")
     startup_conditions["accepted_terms"] = params.get("HasAcceptedTerms") == terms_version
 
