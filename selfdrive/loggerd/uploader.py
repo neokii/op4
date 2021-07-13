b13a046388de785e69f861de7e8ff185df7acc59
Circuit Pro Industries <oricalworks@gmail.com>
2021-07-13 11:32:58 -0400

    Update uploader.py

--- selfdrive/loggerd/uploader.py
+++ selfdrive/loggerd/uploader.py
@@ -224,7 +224,7 @@ def uploader_fn(exit_event):
     if onroad and Params().get_bool('hotspot_on_boot'):
       os.system("service call wifi 37 i32 0 i32 1 &")
       
-    d = uploader.next_file_to_upload(with_raw=allow_raw_upload and offroad)
+    d = uploader.next_file_to_upload(with_raw=allow_raw_upload and on_wifi and offroad)
     if d is None:  # Nothing to upload
       if allow_sleep:
         time.sleep(60 if offroad else 5)
