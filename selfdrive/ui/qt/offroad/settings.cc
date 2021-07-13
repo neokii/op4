798823c953bfda92965a60c4d047e3b347a22c7c
Circuit Pro Industries <oricalworks@gmail.com>
2021-07-12 22:38:20 -0400

    Update settings.cc

--- selfdrive/ui/qt/offroad/settings.cc
+++ selfdrive/ui/qt/offroad/settings.cc
@@ -360,7 +360,7 @@ QWidget * community_panel() {
 
   auto nTune = new ButtonControl("Run nTune AutoTune for lateral.", "nTune");
   QObject::connect(nTune, &ButtonControl::released, [=]() { 
-    if (offroad && ConfirmationDialog::confirm("Run nTune? This Lags.")){
+    if (params.getBool("IsOffroad") && ConfirmationDialog::confirm("Run nTune? This Lags.")){
       std::system("cd /data/openpilot/selfdrive && python ntune.py");
       ConfirmationDialog::confirm("nTune Ran Successfully");     
     }
