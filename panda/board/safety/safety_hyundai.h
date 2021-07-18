a087277b6cbb81ae07bea4c3c91c06cac1bf58f3
Circuit Pro Industries <oricalworks@gmail.com>
2021-07-18 14:36:08 -0400

    Revert "test sonata fix"
    This reverts commit 0bf9637879010f32b7ed333966f7dcb53c6d6ffb.

--- panda/board/safety/safety_hyundai.h
+++ panda/board/safety/safety_hyundai.h
@@ -170,7 +170,7 @@ static int hyundai_rx_hook(CAN_FIFOMailBox_TypeDef *to_push) {
     }
 
     if (addr == 916) {
-      //brake_pressed = (GET_BYTE(to_push, 6) >> 7) != 0;
+      brake_pressed = (GET_BYTE(to_push, 6) >> 7) != 0;
     }
 
     generic_rx_checks((addr == 832));
