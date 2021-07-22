9c0a8133e0602dc48bf48a74800cfae42e766660
Circuit Pro Industries <oricalworks@gmail.com>
2021-07-22 10:07:33 -0400

    Update process_config.py

--- selfdrive/manager/process_config.py
+++ selfdrive/manager/process_config.py
@@ -67,7 +67,7 @@ else:
   PythonProcess("thermald", "selfdrive.thermald.thermald", persistent=True),
   PythonProcess("timezoned", "selfdrive.timezoned", enabled=TICI, persistent=True),
   #PythonProcess("tombstoned", "selfdrive.tombstoned", enabled=not PC, persistent=True),
-  #PythonProcess("updated", "selfdrive.updated", enabled=not PC, persistent=True),
+  PythonProcess("updated", "selfdrive.updated", enabled=not PC, persistent=True),
   #PythonProcess("uploader", "selfdrive.loggerd.uploader", persistent=True),
 ]
 managed_processes = {p.name: p for p in procs}
