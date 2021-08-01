390ac6fb23d4e03afd4275b49b5fa5fcf6f055d0
Circuit Pro Industries <oricalworks@gmail.com>
2021-08-01 08:59:17 -0400

    Update prime.cc

--- selfdrive/ui/qt/widgets/prime.cc
+++ selfdrive/ui/qt/widgets/prime.cc
@@ -29,20 +29,9 @@ void PairingQRWidget::showEvent(QShowEvent *event) {
   refresh();
 }
 
-void PairingQRWidget::refresh(){
-  Params params;
-  QString IMEI = QString::fromStdString(params.get("IMEI"));
-  QString serial = QString::fromStdString(params.get("HardwareSerial"));
-
-  if (std::min(IMEI.length(), serial.length()) <= 5) {
-    qrCode->setText("Error getting serial: contact support");
-    qrCode->setWordWrap(true);
-    qrCode->setStyleSheet(R"(font-size: 60px;)");
-    return;
-  }
+void PairingQRWidget::refresh() {
   QString pairToken = CommaApi::create_jwt({{"pair", true}});
-  
-  QString qrString = IMEI + "--" + serial + "--" + pairToken;
+  QString qrString = "https://connect.comma.ai/?pair=" + pairToken;
   this->updateQrCode(qrString);
 }
 
@@ -75,46 +64,45 @@ PrimeUserWidget::PrimeUserWidget(QWidget* parent) : QWidget(parent) {
   mainLayout->setMargin(0);
   mainLayout->setSpacing(30);
 
-  QLabel* commaPrime = new QLabel("RetroPilot Online");
-  mainLayout->addWidget(commaPrime, 0, Qt::AlignTop);
+  // subscribed prime layout
+  QWidget *primeWidget = new QWidget;
+  primeWidget->setObjectName("primeWidget");
+  QVBoxLayout *primeLayout = new QVBoxLayout(primeWidget);
+  primeLayout->setMargin(0);
+  primeWidget->setContentsMargins(60, 50, 60, 50);
 
-  username = new QLabel();
-  QLabel* RetroPilot = new QLabel("Circuit Pro HKG");
-  mainLayout->addWidget(RetroPilot, 0, Qt::AlignTop);
-  username->setStyleSheet("font-size: 53px;"); // TODO: fit width
+  QLabel* subscribed = new QLabel("✓ SUBSCRIBED");
+  subscribed->setStyleSheet("font-size: 41px; font-weight: bold; color: #86FF4E;");
+  primeLayout->addWidget(subscribed, 0, Qt::AlignTop);
 
-  mainLayout->addWidget(username, 0, Qt::AlignTop);
+  primeLayout->addSpacing(60);
 
-  mainLayout->addSpacing(50);
+  QLabel* commaPrime = new QLabel("comma prime");
+  commaPrime->setStyleSheet("font-size: 75px; font-weight: bold;");
+  primeLayout->addWidget(commaPrime, 0, Qt::AlignTop);
 
-  QLabel* commaPoints = new QLabel("Welcome To The Community!");
-  commaPoints->setStyleSheet(R"(
-    color: #b8b8b8;
-    font-size: 43px
-  )");
+  primeLayout->addSpacing(20);
 
-  QLabel* discordMessage = new QLabel("Please Join Our Discord.");
-  discordMessage->setStyleSheet(R"(
-    color: #b8b8b8;
-    font-size: 43px
-  )");
+  QLabel* connectUrl = new QLabel("CONNECT.COMMA.AI");
+  connectUrl->setStyleSheet("font-size: 41px; font-family: Inter SemiBold; color: #A0A0A0;");
+  primeLayout->addWidget(connectUrl, 0, Qt::AlignTop);
 
-  QLabel* discordLink = new QLabel("https://discord.gg/zWSnqJ6rKD");
-  discordLink->setStyleSheet(R"(
-    color: #b8b8b8;
-    font-size: 40px
-  )");
+  mainLayout->addWidget(primeWidget);
 
-  mainLayout->addWidget(commaPoints, 0, Qt::AlignTop);
-  mainLayout->addWidget(discordMessage, 0, Qt::AlignTop);
-  mainLayout->addWidget(discordLink, 0, Qt::AlignTop);
+  // comma points layout
+  QWidget *pointsWidget = new QWidget;
+  pointsWidget->setObjectName("primeWidget");
+  QVBoxLayout *pointsLayout = new QVBoxLayout(pointsWidget);
+  pointsLayout->setMargin(0);
+  pointsWidget->setContentsMargins(60, 50, 60, 50);
 
-  QLabel* GitHub = new QLabel("https://github.com/Circuit-Pro/openpilot");
-  GitHub->setStyleSheet(R"(
-    font-size: 31px;
-    color: #b8b8b8;
-  )");
-  mainLayout->addWidget(GitHub, 0, Qt::AlignTop);
+  QLabel* commaPoints = new QLabel("COMMA POINTS");
+  commaPoints->setStyleSheet("font-size: 41px; font-family: Inter SemiBold;");
+  pointsLayout->addWidget(commaPoints, 0, Qt::AlignTop);
+
+  points = new QLabel("210");
+  points->setStyleSheet("font-size: 91px; font-weight: bold;");
+  pointsLayout->addWidget(points, 0, Qt::AlignTop);
 
   mainLayout->addWidget(pointsWidget);
 
@@ -123,7 +111,7 @@ PrimeUserWidget::PrimeUserWidget(QWidget* parent) : QWidget(parent) {
   // set up API requests
   std::string dongleId = Params().get("DongleId");
   if (util::is_valid_dongle_id(dongleId)) {
-    std::string url = "https://api.retropilot.org/v1.1/devices/" + dongleId + "/owner";
+    std::string url = "https://api.commadotai.com/v1/devices/" + dongleId + "/owner";
     RequestRepeater *repeater = new RequestRepeater(this, QString::fromStdString(url), "ApiCache_Owner", 6);
     QObject::connect(repeater, &RequestRepeater::receivedResponse, this, &PrimeUserWidget::replyFinished);
   }
@@ -145,21 +133,30 @@ PrimeAdWidget::PrimeAdWidget(QWidget* parent) : QFrame(parent) {
   main_layout->setContentsMargins(80, 90, 80, 60);
   main_layout->setSpacing(0);
 
-  main_layout->addWidget(new QLabel("RetroPilot is Down."), 1, Qt::AlignTop);
+  QLabel *upgrade = new QLabel("Upgrade Now");
+  upgrade->setStyleSheet("font-size: 75px; font-weight: bold;");
+  main_layout->addWidget(upgrade, 0, Qt::AlignTop);
+  main_layout->addSpacing(50);
 
-  QLabel* description = new QLabel("If you are seeing this somethings not right!");
-  description->setStyleSheet(R"(
-    font-size: 50px;
-    color: #b8b8b8;
-  )");
+  QLabel *description = new QLabel("Become a comma prime member at connect.comma.ai");
+  description->setStyleSheet("font-size: 60px; font-weight: light; color: white;");
   description->setWordWrap(true);
-  main_layout->addWidget(description, 2, Qt::AlignTop);
+  main_layout->addWidget(description, 0, Qt::AlignTop);
 
-  QVector<QString> features = {"Not Right", "RetroPilot", "Services Down"};
-  for (auto &f: features) {
-    QLabel* feature = new QLabel(f);
-    feature->setStyleSheet(R"(font-size: 40px;)");
-    main_layout->addWidget(feature, 0, Qt::AlignBottom);
+  main_layout->addStretch();
+
+  QLabel *features = new QLabel("PRIME FEATURES:");
+  features->setStyleSheet("font-size: 41px; font-weight: bold; color: #E5E5E5;");
+  main_layout->addWidget(features, 0, Qt::AlignBottom);
+  main_layout->addSpacing(30);
+
+  QVector<QString> bullets = {"Remote access", "14 days of storage", "Developer perks"};
+  for (auto &b: bullets) {
+    const QString check = "<b><font color='#465BEA'>✓</font></b> ";
+    QLabel *l = new QLabel(check + b);
+    l->setAlignment(Qt::AlignLeft);
+    l->setStyleSheet("font-size: 50px; margin-bottom: 15px;");
+    main_layout->addWidget(l, 0, Qt::AlignBottom);
   }
 
   setStyleSheet(R"(
@@ -182,13 +179,9 @@ SetupWidget::SetupWidget(QWidget* parent) : QFrame(parent) {
   finishRegistationLayout->setContentsMargins(30, 75, 30, 45);
   finishRegistationLayout->setSpacing(0);
 
-  QLabel* registrationDescription = new QLabel("Pair your device at api.RetroPilot.org/useradmin");
-  registrationDescription->setWordWrap(true);
-  registrationDescription->setAlignment(Qt::AlignCenter);
-  registrationDescription->setStyleSheet(R"(
-    font-size: 55px;
-    font-weight: 400;
-  )");
+  QLabel* registrationTitle = new QLabel("Finish Setup");
+  registrationTitle->setStyleSheet("font-size: 75px; font-weight: bold; margin-left: 55px;");
+  finishRegistationLayout->addWidget(registrationTitle);
 
   finishRegistationLayout->addSpacing(30);
 
@@ -264,7 +257,7 @@ SetupWidget::SetupWidget(QWidget* parent) : QFrame(parent) {
   // set up API requests
   std::string dongleId = Params().get("DongleId");
   if (util::is_valid_dongle_id(dongleId)) {
-    std::string url = "https://api.retropilot.org/v1.1/devices/" + dongleId + "/";
+    std::string url = "https://api.commadotai.com/v1.1/devices/" + dongleId + "/";
     RequestRepeater* repeater = new RequestRepeater(this, QString::fromStdString(url), "ApiCache_Device", 5);
 
     QObject::connect(repeater, &RequestRepeater::receivedResponse, this, &SetupWidget::replyFinished);
