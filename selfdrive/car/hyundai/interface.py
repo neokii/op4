bd313dfed0fcf240bd863ebc2d95fdc8686e1730
Circuit Pro Industries <oricalworks@gmail.com>
2021-08-03 16:25:43 -0400

    Update interface.py

--- selfdrive/car/hyundai/interface.py
+++ selfdrive/car/hyundai/interface.py
@@ -534,8 +534,8 @@ class CarInterface(CarInterfaceBase):
 
     if self.CC.longcontrol and self.CS.cruise_unavail:
       events.add(EventName.brakeUnavailable)
-    #if abs(ret.steeringAngleDeg) > 90. and EventName.steerTempUnavailable not in events.events:
-    #  events.add(EventName.steerTempUnavailable)
+    if abs(ret.steeringAngleDeg) > maxSteeringAngleDeg and EventName.steerTempUnavailable not in events.events:
+      events.add(EventName.steerTempUnavailable)
     if self.low_speed_alert and not self.CS.mdps_bus:
       events.add(EventName.belowSteerSpeed)
     if self.CC.turning_indicator_alert:
