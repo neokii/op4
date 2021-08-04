dde1cdfde65b7426e743b75647e9734c8c581aa8
Circuit Pro Industries <oricalworks@gmail.com>
2021-08-04 14:59:12 -0400

    Update events.py

--- selfdrive/controls/lib/events.py
+++ selfdrive/controls/lib/events.py
@@ -194,7 +194,7 @@ def below_steer_speed_alert(CP: car.CarParams, sm: messaging.SubMaster, metric: 
     Priority.MID, VisualAlert.steerRequired, AudibleAlert.chimePrompt, 0., 0.4, .3)
 
 #JPR
-def FTPMS(CP: car.CarParams, CS: car.CarState, sm: messaging.SubMaster, metric: bool) -> Alert:
+def fTPMS(CP: car.CarParams, CS: car.CarState, sm: messaging.SubMaster, metric: bool) -> Alert:
   if CS.tpmsFl < CP.minFTP:
     TPF = int(CS.tpmsFl)
     tpms = "Front Left"
@@ -208,7 +208,7 @@ def FTPMS(CP: car.CarParams, CS: car.CarState, sm: messaging.SubMaster, metric: 
     AlertStatus.userPrompt, AlertSize.mid,
     Priority.MID, VisualAlert.steerRequired, AudibleAlert.chimePrompt, 0., 0.4, .3)
 
-def RTPMS(CP: car.CarParams, CS: car.CarState, sm: messaging.SubMaster, metric: bool) -> Alert:
+def rTPMS(CP: car.CarParams, CS: car.CarState, sm: messaging.SubMaster, metric: bool) -> Alert:
   if CS.tpmsRl < CP.tpmsRl:
     TPR = int(CS.tpmsRl)
     tpms = "Rear Left"
