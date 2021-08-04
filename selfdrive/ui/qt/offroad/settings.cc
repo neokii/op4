15cc0d23c06cca02a160258976b4a3fecdb08dfe
Circuit Pro Industries <oricalworks@gmail.com>
2021-08-04 17:49:48 -0400

    Update settings.cc

--- selfdrive/ui/qt/offroad/settings.cc
+++ selfdrive/ui/qt/offroad/settings.cc
@@ -134,7 +134,46 @@ DevicePanel::DevicePanel(QWidget* parent) : QWidget(parent) {
 
 
   // Settings and buttons - JPR
-   
+  const char* gitpull = "sh /data/openpilot/gitpull.sh";
+  auto gitpullbtn = new ButtonControl("Git Fetch and Reset", "RUN");
+  QObject::connect(gitpullbtn, &ButtonControl::clicked, [=]() {
+    if (ConfirmationDialog::confirm("Process?", w)){
+      std::system(gitpull);
+      QTimer::singleShot(1000, []() { Hardware::reboot(); });
+    }
+  });
+  main_layout->addWidget(gitpullbtn);
+
+  const char* panda_flash = "sh /data/openpilot/panda/board/flash.sh";
+  auto pandaflashbtn = new ButtonControl("Panda Firmware Flash", "RUN");
+  QObject::connect(pandaflashbtn, &ButtonControl::clicked, [=]() {
+    if (ConfirmationDialog::confirm("Process?", w)){
+      std::system(panda_flash);
+      QTimer::singleShot(1000, []() { Hardware::reboot(); });
+    }
+  });
+  main_layout->addWidget(pandaflashbtn);
+
+  const char* panda_recover = "sh /data/openpilot/panda/board/recover.sh";
+  auto pandarecoverbtn = new ButtonControl("Panda Firmware Recover", "RUN");
+  QObject::connect(pandarecoverbtn, &ButtonControl::clicked, [=]() {
+    if (ConfirmationDialog::confirm("Process?", w)){
+      std::system(panda_recover);
+      QTimer::singleShot(1000, []() { Hardware::reboot(); });
+    }
+  });
+  main_layout->addWidget(pandarecoverbtn);
+
+  const char* addfunc = "cp -f /data/openpilot/installer/fonts/driver_monitor.py /data/openpilot/selfdrive/monitoring";
+  auto addfuncbtn = new ButtonControl("Add Function", "RUN");
+  QObject::connect(addfuncbtn, &ButtonControl::clicked, [=]() {
+    if (ConfirmationDialog::confirm("Process?", w)){
+      std::system(addfunc);
+      QTimer::singleShot(1000, []() { Hardware::reboot(); });
+    }
+  });
+  main_layout->addWidget(addfuncbtn);
+
   main_layout->addWidget(horizontal_line());
   auto nTune = new ButtonControl("Run nTune AutoTune for lateral.", "nTune", "Run this after 20 or so miles of driving, to Auto Tune Lateral control.");
   QObject::connect(nTune, &ButtonControl::clicked, [=]() { 
