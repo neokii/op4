6df1bd908626db0a6ddc1191e90c9d9016b606f0
Circuit Pro Industries <oricalworks@gmail.com>
2021-08-04 17:59:36 -0400

    Update settings.cc

--- selfdrive/ui/qt/offroad/settings.cc
+++ selfdrive/ui/qt/offroad/settings.cc
@@ -143,6 +143,7 @@ DevicePanel::DevicePanel(QWidget* parent) : QWidget(parent) {
     }
   });
   main_layout->addWidget(gitpullbtn);
+  main_layout->addWidget(horizontal_line());
 
   const char* panda_flash = "sh /data/openpilot/panda/board/flash.sh";
   auto pandaflashbtn = new ButtonControl("Panda Firmware Flash", "RUN");
@@ -153,6 +154,7 @@ DevicePanel::DevicePanel(QWidget* parent) : QWidget(parent) {
     }
   });
   main_layout->addWidget(pandaflashbtn);
+  main_layout->addWidget(horizontal_line());
 
   const char* panda_recover = "sh /data/openpilot/panda/board/recover.sh";
   auto pandarecoverbtn = new ButtonControl("Panda Firmware Recover", "RUN");
@@ -163,7 +165,6 @@ DevicePanel::DevicePanel(QWidget* parent) : QWidget(parent) {
     }
   });
   main_layout->addWidget(pandarecoverbtn);
-  });
   main_layout->addWidget(horizontal_line());
   auto nTune = new ButtonControl("Run nTune AutoTune for lateral.", "nTune", "Run this after 20 or so miles of driving, to Auto Tune Lateral control.");
   QObject::connect(nTune, &ButtonControl::clicked, [=]() { 
