7adf8d3d8bf27313e6af6c7e59257a29954a5bb3
Circuit Pro Industries <oricalworks@gmail.com>
2021-08-04 18:07:24 -0400

    Update prime.cc

--- selfdrive/ui/qt/widgets/prime.cc
+++ selfdrive/ui/qt/widgets/prime.cc
@@ -103,7 +103,7 @@ PrimeUserWidget::PrimeUserWidget(QWidget* parent) : QWidget(parent) {
 
 
   QLabel* points = new QLabel("https://discord.gg/bXGF7r8EPW");
-  points->setStyleSheet("font-size: 42px; font-weight: bold;");
+  points->setStyleSheet("font-size: 39px; font-weight: bold;");
   pointsLayout->addWidget(points, 0, Qt::AlignTop);
 
   mainLayout->addWidget(pointsWidget);
