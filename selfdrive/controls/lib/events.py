2f9deb5ef41cf1b3747423239da1186689df1cd9
Circuit Pro Industries <oricalworks@gmail.com>
2021-08-04 22:03:19 -0400

    Update events.py

--- selfdrive/controls/lib/events.py
+++ selfdrive/controls/lib/events.py
@@ -6,7 +6,7 @@ import cereal.messaging as messaging
 from common.realtime import DT_CTRL
 from selfdrive.config import Conversions as CV
 from selfdrive.locationd.calibrationd import MIN_SPEED_FILTER
-from selfdrive.car.hyundai.interface import CarInterfaceBase
+
 
 AlertSize = log.ControlsState.AlertSize
 AlertStatus = log.ControlsState.AlertStatus
