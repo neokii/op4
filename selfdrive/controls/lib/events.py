0c00ef0c244f099fded89870e787015bb1221280
Circuit Pro Industries <oricalworks@gmail.com>
2021-08-05 19:42:46 -0400

    Update events.py

--- selfdrive/controls/lib/events.py
+++ selfdrive/controls/lib/events.py
@@ -194,31 +194,31 @@ def below_steer_speed_alert(CP: car.CarParams, sm: messaging.SubMaster, metric: 
     AlertStatus.userPrompt, AlertSize.mid,
     Priority.MID, VisualAlert.steerRequired, AudibleAlert.chimePrompt, 0., 0.4, .3)
 #JPR
-def flTPMS(CS: car.CarState, sm: messaging.SubMaster, metric: bool) -> Alert:
+def flTPMS(CS, sm: messaging.SubMaster, metric: bool) -> Alert:
   return Alert(
     "LOW FRONT LEFT TIRE PRESSURE",
-    "(%d) PSI" % car.CarState.tpmsFl,
+    "(%d) PSI" % CS.CarState.tpmsFl,
     AlertStatus.userPrompt, AlertSize.mid,
     Priority.MID, VisualAlert.steerRequired, AudibleAlert.chimePrompt, 0., 0.4, .3)
 
-def frTPMS(CS: car.CarState, sm: messaging.SubMaster, metric: bool) -> Alert:
+def frTPMS(CS, sm: messaging.SubMaster, metric: bool) -> Alert:
   return Alert(
     "LOW FRONT RIGHT TIRE PRESSURE",
-    "(%d) PSI" % car.CarState.tpmsFr,
+    "(%d) PSI" % CS.CarState.tpmsFr,
     AlertStatus.userPrompt, AlertSize.mid,
     Priority.MID, VisualAlert.steerRequired, AudibleAlert.chimePrompt, 0., 0.4, .3)
 
-def rlTPMS(CS: car.CarState, sm: messaging.SubMaster, metric: bool) -> Alert:
+def rlTPMS(CS, sm: messaging.SubMaster, metric: bool) -> Alert:
   return Alert(  
     "LOW REAR LEFT TIRE PRESSURE",
-    "(%d) PSI" % car.CarState.tpmsRl,
+    "(%d) PSI" % CS.CarState.tpmsRl,
     AlertStatus.userPrompt, AlertSize.mid,
     Priority.MID, VisualAlert.steerRequired, AudibleAlert.chimePrompt, 0., 0.4, .3)
 
-def rrTPMS(CS: car.CarState, sm: messaging.SubMaster, metric: bool) -> Alert:
+def rrTPMS(CS, sm: messaging.SubMaster, metric: bool) -> Alert:
   return Alert(
     "LOW REAR RIGHT TIRE PRESSURE",
-    "(%d) PSI" % car.CarState.tpmsRr,
+    "(%d) PSI" % CS.CarState.tpmsRr,
     AlertStatus.userPrompt, AlertSize.mid,
     Priority.MID, VisualAlert.steerRequired, AudibleAlert.chimePrompt, 0., 0.4, .3)
 
