7055e9eec108006347007209059dbe7e6a65aa62
Circuit Pro Industries <oricalworks@gmail.com>
2021-08-06 21:09:18 -0400

    Revert "Revert "Merge branch 'hkg_088_dev' into dev""
    This reverts commit 000fb6e6b2d09faf5da8f7d9318e7bf0a935c9c8.

--- selfdrive/controls/lib/lead_mpc.py
+++ selfdrive/controls/lib/lead_mpc.py
@@ -10,10 +10,10 @@ from selfdrive.swaglog import cloudlog
 from selfdrive.config import Conversions as CV
 
 CRUISE_GAP_BP = [1., 2., 3., 4.]
-CRUISE_GAP_V = [1.3, 1.6, 2.1, 2.3]
+CRUISE_GAP_V = [1.3, 1.4, 1.8, 2.2]
 
-AUTO_TR_BP = [20.*CV.KPH_TO_MS, 80.*CV.KPH_TO_MS, 130.*CV.KPH_TO_MS]
-AUTO_TR_V = [1.25, 1.325, 1.4]
+AUTO_TR_BP = [20.*CV.KPH_TO_MS, 50.*CV.KPH_TO_MS, 80.*CV.KPH_TO_MS, 130.*CV.KPH_TO_MS]
+AUTO_TR_V = [1.2, 1.3, 1.4, 1.5]
 
 AUTO_TR_ENABLED = True
 AUTO_TR_CRUISE_GAP = 2
