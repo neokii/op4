f4a508777075527e6f15599a2d937b8c45966509
Circuit Pro Industries <oricalworks@gmail.com>
2021-08-10 19:32:18 -0400

    Revert "Update safety_hyundai_community.h"
    This reverts commit 44862bdd92bc694e0a2b3a0de4e04df9fa0bca7f.

--- panda/board/safety/safety_hyundai_community.h
+++ panda/board/safety/safety_hyundai_community.h
@@ -222,7 +222,7 @@ static int hyundai_community_fwd_hook(int bus_num, CAN_FIFOMailBox_TypeDef *to_f
   int addr = GET_ADDR(to_fwd);
   int fwd_to_bus1 = -1;
   //if (HKG_forward_bus1 || HKG_forward_obd){fwd_to_bus1 = 1;}
-  if ((HKG_forward_bus1 || HKG_forward_obd) && (addr == 832 || addr == 544 || addr == 790 || addr == 273 || addr == 1292 || addr == 1136 || addr == 870)){fwd_to_bus1 = 1;}
+  if ((HKG_forward_bus1 || HKG_forward_obd) && (addr == 1345 || addr == 832 || addr == 544 || addr == 790 || addr == 273 || addr == 1292 || addr == 1136 || addr == 870)){fwd_to_bus1 = 1;}
   // forward cam to ccan and viceversa, except lkas cmd
   if (HKG_forward_bus2) {
     if (bus_num == 0) {
