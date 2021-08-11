824768bfbd33dd29fcacd24192461f5285878940
Circuit Pro Industries <oricalworks@gmail.com>
2021-08-11 05:39:43 -0400

    Update carcontroller.py

--- selfdrive/car/hyundai/carcontroller.py
+++ selfdrive/car/hyundai/carcontroller.py
@@ -5,7 +5,7 @@ from common.numpy_fast import clip
 from selfdrive.car import apply_std_steer_torque_limits
 from selfdrive.car.hyundai.hyundaican import create_lkas11, create_clu11, \
   create_scc11, create_scc12, create_scc13, create_scc14, \
-  create_mdps12, create_lfahda_mfc, create_hda_mfc, create_spas11, create_spas12, create_ems11
+  create_mdps12, create_lfahda_mfc, create_hda_mfc, create_spas11, create_spas12, create_ems_366
 from selfdrive.car.hyundai.scc_smoother import SccSmoother
 from selfdrive.car.hyundai.values import Buttons, CAR, FEATURES, CarControllerParams, FEATURES
 from opendbc.can.packer import CANPacker
