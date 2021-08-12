2285cffc04675119952ebb6c9bdc09108033bb92
Circuit Pro Industries <oricalworks@gmail.com>
2021-08-12 14:42:49 -0400

    Update carcontroller.py

--- selfdrive/car/hyundai/carcontroller.py
+++ selfdrive/car/hyundai/carcontroller.py
@@ -122,7 +122,7 @@ class CarController():
       else:
         rate_limit = interp(CS.out.vEgo, ANGLE_DELTA_BP, ANGLE_DELTA_VU)
 
-      apply_angle = (clip(apply_angle, self.last_apply_angle - rate_limit, self.last_apply_angle + rate_limit) * 0.93)
+      apply_angle = clip(apply_angle, self.last_apply_angle - rate_limit, self.last_apply_angle + rate_limit)
 
       self.last_apply_angle = apply_angle
 
