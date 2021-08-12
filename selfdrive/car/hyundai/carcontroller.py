b8ff63f74d7825358af0913434922f36b6e904ad
Circuit Pro Industries <oricalworks@gmail.com>
2021-08-12 00:11:46 -0400

    Update tune

--- selfdrive/car/hyundai/carcontroller.py
+++ selfdrive/car/hyundai/carcontroller.py
@@ -21,8 +21,8 @@ VisualAlert = car.CarControl.HUDControl.VisualAlert
 STEER_ANG_MAX = 220          # SPAS Max Angle
 # nissan limits values
 ANGLE_DELTA_BP = [0., 5., 15.]		# speed m/s
-ANGLE_DELTA_V = [5., .6, .8]     # windup limit
-ANGLE_DELTA_VU = [5., 3.5, 0.4]   # unwind limit
+ANGLE_DELTA_V = [3., .4, .6]     # windup limit
+ANGLE_DELTA_VU = [5., 2.5, 0.3]   # unwind limit
 DRIVER_TORQUE_THRESHOLD = 250
 
 def accel_hysteresis(accel, accel_steady):
