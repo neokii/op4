4e2f7a3f750a69e33060f5892c3275f7fdd05403
Circuit Pro Industries <oricalworks@gmail.com>
2021-08-17 15:43:48 -0400

    test

--- selfdrive/car/hyundai/carstate.py
+++ selfdrive/car/hyundai/carstate.py
@@ -90,8 +90,9 @@ class CarState(CarStateBase):
 
     ret.standstill = ret.vEgoRaw < 0.1
 
-    ret.steeringAngleDeg = cp_sas.vl["SAS11"]['SAS_Angle']
-    ret.steeringRateDeg = cp_sas.vl["SAS11"]['SAS_Speed']
+    steeringAngleDeg = cp_sas.vl["SAS11"]['SAS_Angle']
+    steeringRateDeg = cp_sas.vl["SAS11"]['SAS_Speed']
+    ret.steeringAngleDeg = steeringAngleDeg * (182 / 200)
     ret.yawRate = cp.vl["ESP12"]['YAW_RATE']
     ret.leftBlinker, ret.rightBlinker = self.update_blinker_from_lamp(50, cp.vl["CGW1"]['CF_Gway_TurnSigLh'],
                                                             cp.vl["CGW1"]['CF_Gway_TurnSigRh'])
