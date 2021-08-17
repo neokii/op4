38dca623d63f7d36e1c29d362167cc28b29e3413
Circuit Pro Industries <oricalworks@gmail.com>
2021-08-16 20:45:45 -0400

    Update carcontroller.py

--- selfdrive/car/hyundai/carcontroller.py
+++ selfdrive/car/hyundai/carcontroller.py
@@ -24,8 +24,9 @@ ANGLE_DELTA_BP = [0., 16., 36.]		# speed m/s
 ANGLE_DELTA_V = [1, .5, .1]     # windup limit
 ANGLE_DELTA_VU = [1.5, 1.0, 0.2]   # unwind limit
 DRIVER_TORQUE_THRESHOLD = 350
-STEER_DEADBAND1 = 2.0 # For Wobbly Steering at high speed
-STEER_DEADBAND2 = 1.8 # For Wobbly Steering at high speed
+STEER_DEADBAND1 = 2.3 # For Wobbly Steering at high speed 40mph
+STEER_DEADBAND2 = 2.0 # For Wobbly Steering at high speed 50mph
+STEER_DEADBAND3 = 1.8 # For Wobbly Steering at high speed 65mph
 
 def accel_hysteresis(accel, accel_steady):
   # for small accel oscillations within ACCEL_HYST_GAP, don't change the accel command
@@ -129,8 +130,10 @@ class CarController():
       #Fix SPAS high speed wobble may need tuning per vehicle- JPR
       if Params().get_bool('SteerDeadBand'):
         apply_angle1 = clip(apply_angle, self.last_apply_angle - rate_limit, self.last_apply_angle + rate_limit)
-        if CS.out.vEgo > (60 * CV.MPH_TO_MS) and STEER_DEADBAND2 >= apply_angle >= -STEER_DEADBAND2:
+        if CS.out.vEgo > (65 * CV.MPH_TO_MS) and STEER_DEADBAND3 >= apply_angle >= -STEER_DEADBAND3:
           apply_angle = (apply_angle1 * 0.80)
+        if CS.out.vEgo > (50 * CV.MPH_TO_MS) and STEER_DEADBAND2 >= apply_angle >= -STEER_DEADBAND2:
+          apply_angle = (apply_angle1 * 0.83)
         elif CS.out.vEgo > (40 * CV.MPH_TO_MS) and STEER_DEADBAND1 >= apply_angle >= -STEER_DEADBAND1:
           apply_angle = (apply_angle1 * 0.86)
 
