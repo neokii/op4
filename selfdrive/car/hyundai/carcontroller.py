ec4b3e7f9dafc9a527cf76c6a8faabe88e299402
Circuit Pro Industries <oricalworks@gmail.com>
2021-08-16 22:53:02 -0400

    Update carcontroller.py

--- selfdrive/car/hyundai/carcontroller.py
+++ selfdrive/car/hyundai/carcontroller.py
@@ -130,10 +130,10 @@ class CarController():
       if Params().get_bool('SteerDeadBand'):
         apply_angle1 = clip(apply_angle, self.last_apply_angle - rate_limit, self.last_apply_angle + rate_limit)
         if CS.out.vEgo > (80 * CV.MPH_TO_MS) and STEER_DEADBAND >= apply_angle >= -STEER_DEADBAND:
-          apply_angle2 = (apply_angle1 * 0.92)
+          apply_angle2 = (apply_angle1 * 0.95)
           apply_angle = (apply_angle2 + self.last_apply_angle + self.LA1 + self.LA2) / 4
         if CS.out.vEgo > (70 * CV.MPH_TO_MS) and STEER_DEADBAND >= apply_angle >= -STEER_DEADBAND:
-          apply_angle2 = (apply_angle1 * 0.89)
+          apply_angle2 = (apply_angle1 * 0.92)
           apply_angle = (apply_angle2 + self.last_apply_angle + self.LA1 + self.LA2) / 4
         elif CS.out.vEgo > (60 * CV.MPH_TO_MS) and STEER_DEADBAND >= apply_angle >= -STEER_DEADBAND:
           apply_angle2 = (apply_angle1 * 0.865)
