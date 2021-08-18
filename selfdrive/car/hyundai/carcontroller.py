c6b65e5227489458f8fec4a723b1c90762b3dfde
Circuit Pro Industries <oricalworks@gmail.com>
2021-08-18 10:38:18 -0400

    Update carcontroller.py

--- selfdrive/car/hyundai/carcontroller.py
+++ selfdrive/car/hyundai/carcontroller.py
@@ -276,7 +276,10 @@ class CarController():
     self.steer_rate_limited = new_steer != apply_steer
     # SPAS limit angle extremes for safety
     if CS.spas_enabled:
-      apply_angle = actuators.steeringAngleDeg*math.pi/180.
+      if Params().get_bool('spas_always'):
+        apply_angle = actuators.steeringAngleDeg*math.pi/180.
+      else:
+        apply_angle = actuators.steer
       if self.last_apply_angle * apply_angle > 0. and abs(apply_angle) > abs(self.last_apply_angle):
         rate_limit = interp(CS.out.vEgo, ANGLE_DELTA_BP, ANGLE_DELTA_V)
       else:
