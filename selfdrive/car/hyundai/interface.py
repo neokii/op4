28cea20c4101be95da5de4beb976f197d54e7765
Circuit Pro Industries <oricalworks@gmail.com>
2021-08-18 19:45:47 -0400

    Update interface.py

--- selfdrive/car/hyundai/interface.py
+++ selfdrive/car/hyundai/interface.py
@@ -619,7 +619,6 @@ class CarInterface(CarInterfaceBase):
     if abs(ret.steeringAngleDeg) > self.CP.maxSteeringAngleDeg and EventName.steerSaturated not in events.events:
       events.add(EventName.steerSaturated)
       self.steerSaturated = True
-      ret.steerSaturated = True
     if self.low_speed_alert and not self.CS.mdps_bus:
       events.add(EventName.belowSteerSpeed)
     if self.CC.turning_indicator_alert:
