e1259a074ff9f49bf76a543549d6f15d543dd4df
Circuit Pro Industries <oricalworks@gmail.com>
2021-08-19 17:54:54 -0400

    Update interface.py

--- selfdrive/car/hyundai/interface.py
+++ selfdrive/car/hyundai/interface.py
@@ -626,7 +626,8 @@ class CarInterface(CarInterfaceBase):
     #  events.add(EventName.buttonCancel)
     if self.mad_mode_enabled and EventName.pedalPressed in events.events:
       events.events.remove(EventName.pedalPressed)
-    if self.CS.mdps11_stat == 7 or self.CS.mdps11_stat == 6: # need to alert driver when SPAS abort or fail.
+
+    if self.CS.mdps11_stat == 7 or self.CS.mdps11_stat == 6: # We need to alert driver when SPAS abort or fail.
       events.add(EventName.steerTempUnavailable)
 
   # handle button presses
