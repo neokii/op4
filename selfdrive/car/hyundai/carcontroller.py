b1922bc1076bcee615d8d8ebcb4397955980e392
Circuit Pro Industries <oricalworks@gmail.com>
2021-08-20 19:28:31 -0400

    Update carcontroller.py

--- selfdrive/car/hyundai/carcontroller.py
+++ selfdrive/car/hyundai/carcontroller.py
@@ -153,6 +153,9 @@ class CarController():
     if enabled and spas_active and TQ <= CS.out.steeringWheelTorque <= -TQ:
       lkas_active = False
       spas_active = False
+    if enabled and not spas_active and (TQ / 0.5) <= CS.out.steeringWheelTorque <= (-TQ / 0.5):
+      spas_active = False
+      lkas_active = True
     
     UseSMDPS = Params().get_bool('UseSMDPSHarness')
     if Params().get_bool('LongControlEnabled'):
