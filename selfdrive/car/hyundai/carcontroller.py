23ac8ea3eaaafb2d22a68b3efbff1a08f7285579
Circuit Pro Industries <oricalworks@gmail.com>
2021-08-20 04:50:50 -0400

    Update carcontroller.py

--- selfdrive/car/hyundai/carcontroller.py
+++ selfdrive/car/hyundai/carcontroller.py
@@ -146,7 +146,7 @@ class CarController():
     spas_active = CS.spas_enabled and enabled and (self.spas_always or CS.out.vEgo < SPAS_SWITCH)
     lkas_active = enabled and abs(CS.out.steeringAngleDeg) < CS.CP.maxSteeringAngleDeg and not spas_active
 
-    if enabled and spas_active and -TQ <= CS.out.steeringWheelTorque >= TQ:
+    if enabled and spas_active and TQ <= CS.out.steeringWheelTorque <= -TQ:
       lkas_active = False
       spas_active = False
       
