311f66c96c8e2e5d4dbec9fbbbcde178ca6c762c
Circuit Pro Industries <oricalworks@gmail.com>
2021-08-20 01:12:39 -0400

    Update carcontroller.py

--- selfdrive/car/hyundai/carcontroller.py
+++ selfdrive/car/hyundai/carcontroller.py
@@ -391,10 +391,14 @@ class CarController():
 
         if CS.mdps11_stat == 7 and self.en_cnt > 2:
           self.en_spas = 3
+          self.en_cnt = self.en_cnt + 1
 
         if CS.mdps11_stat == 3 and self.en_cnt > 5:
           self.en_spas = 2
           self.en_cnt = 0
+          if CS.mdps11_stat == 2:
+            self.en_spas = 3 # Switch to State 3, and get Ready to Assist(Steer). JPR
+
 
         if CS.mdps11_stat == 6 and self.mdps11_stat_last == 7: # Failed to Assist and Steer, Set state back to 2 for a new request. JPR
           self.en_spas = 2
