23e4300b9ab1367ec783d7128784c94235c77eb3
Circuit Pro Industries <oricalworks@gmail.com>
2021-08-19 20:05:07 -0400

    temporary remove steer unavailable

--- selfdrive/car/hyundai/interface.py
+++ selfdrive/car/hyundai/interface.py
@@ -627,8 +627,8 @@ class CarInterface(CarInterfaceBase):
     if self.mad_mode_enabled and EventName.pedalPressed in events.events:
       events.events.remove(EventName.pedalPressed)
 
-    if self.CS.mdps11_stat == 7 or self.CS.mdps11_stat == 6: # We need to alert driver when SPAS abort or fail.
-      events.add(EventName.steerTempUnavailable)
+    #if self.CS.mdps11_stat == 7 or self.CS.mdps11_stat == 6: # We need to alert driver when SPAS abort or fail.
+    #  events.add(EventName.steerTempUnavailable)
 
   # handle button presses
     for b in ret.buttonEvents:
