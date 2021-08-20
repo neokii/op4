4a6afe2948c6f51d034c358fdd1d2deceb7b4f3d
Circuit Pro Industries <oricalworks@gmail.com>
2021-08-19 20:08:54 -0400

    Update carcontroller.py

--- selfdrive/car/hyundai/carcontroller.py
+++ selfdrive/car/hyundai/carcontroller.py
@@ -532,12 +532,12 @@ class CarController():
           self.en_cnt = 0
 
         if self.en_spas == 7 and self.en_cnt >= 8: # if MDPS stat 7 or 6 start new request. JPR
-          self.en_spas = 2 # previously 3 but we need to start a new request with state 2. JPR
+          self.en_spas = 3 # previously 3 but we need to start a new request with state 2. JPR
           self.en_cnt = 0
 
-        if CS.mdps11_stat == 2: # when MDPS stat change to 2, it's processed new request state and ready to move to state 3 SPAS ready. JPR
-          self.en_spas = 3 # we need to change from starting a new request with state 2 to a spas ready state which is state 3. JPR
-          self.en_cnt = 0
+        #if CS.mdps11_stat == 2: # when MDPS stat change to 2, it's processed new request state and ready to move to state 3 SPAS ready. JPR
+        #  self.en_spas = 3 # we need to change from starting a new request with state 2 to a spas ready state which is state 3. JPR
+        #  self.en_cnt = 0
 
         if self.en_cnt < 8 and spas_active:
           self.en_spas = 4
