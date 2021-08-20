6aa75e99539ab0998fea0cfe7c0f7d26a3b3ae85
Circuit Pro Industries <oricalworks@gmail.com>
2021-08-19 23:01:18 -0400

    Update carcontroller.py

--- selfdrive/car/hyundai/carcontroller.py
+++ selfdrive/car/hyundai/carcontroller.py
@@ -381,17 +381,16 @@ class CarController():
         if CS.mdps11_stat == 6: # and self.mdps11_stat_last == 7: # Failed to Assist and Steer, Set state back to 2 for a new request. JPR
           self.en_spas = 2
           self.en_cnt = 0
-
-
+          
         if self.en_spas == 7 and self.en_cnt >= 8: # if MDPS stat 7 or 6 start new request. JPR
           self.en_spas = 3 # get Ready to Assist and steer.
           self.en_cnt = 0
         
-        if CS.mdps11_stat == 2 and enabled:
-          self.en_spas = 3 # Switch to State 3, and get Ready to Assist(Steer). JPR
-        
         if CS.mdps11_stat == 8: #MDPS ECU Fails to get into state 3 and ready for state 5. JPR
           self.en_spas = 2
+
+        if CS.mdps11_stat == 2 and enabled:
+          self.en_spas = 3 # Switch to State 3, and get Ready to Assist(Steer). JPR
 
         if self.en_cnt < 8 and spas_active:
           self.en_spas = 4
