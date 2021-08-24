65f16f7ec1cc3a539ab60cb7f517f6e3ffa645ec
Circuit Pro Industries <oricalworks@gmail.com>
2021-08-24 19:53:07 -0400

    test

--- selfdrive/car/hyundai/carcontroller.py
+++ selfdrive/car/hyundai/carcontroller.py
@@ -149,12 +149,12 @@ class CarController():
 
     spas_active = CS.spas_enabled and enabled and (self.spas_always or CS.out.vEgo < SPAS_SWITCH) 
     lkas_active = enabled and abs(CS.out.steeringAngleDeg) < CS.CP.maxSteeringAngleDeg and not spas_active
-    #self.override = enabled and spas_active and TQ <= CS.out.steeringWheelTorque <= -TQ
+    self.override = enabled and spas_active and TQ <= CS.out.steeringWheelTorque <= -TQ
     if not lkas_active:
       apply_steer = 0
     if enabled and TQ <= CS.out.steeringWheelTorque <= -TQ:
       spas_active = False
-      #self.override = True
+      self.override = True
   
     
     UseSMDPS = Params().get_bool('UseSMDPSHarness')
