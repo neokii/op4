db9c4b8659ccaca7c67158e140034225055f089a
Circuit Pro Industries <oricalworks@gmail.com>
2021-08-24 15:39:56 -0400

    Update carstate.py

--- selfdrive/car/hyundai/carstate.py
+++ selfdrive/car/hyundai/carstate.py
@@ -347,6 +347,8 @@ class CarState(CarStateBase):
       ("PRESSURE_FR", "TPMS11", 0),
       ("PRESSURE_RL", "TPMS11", 0),
       ("PRESSURE_RR", "TPMS11", 0),
+
+      ("SpeedLim_Nav_Clu", "Navi_HU", 0),
     ]
 
     checks = [
