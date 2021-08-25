d7652d0822d18adeb5d842019f1df77674b081a5
Circuit Pro Industries <oricalworks@gmail.com>
2021-08-25 14:11:58 -0400

    Update carcontroller.py

--- selfdrive/car/hyundai/carcontroller.py
+++ selfdrive/car/hyundai/carcontroller.py
@@ -98,7 +98,7 @@ class CarController():
       self.en_spas = 2
       self.mdps11_stat_last = 0
       self.spas_always = Params().get_bool('spasAlways')
-      self.spas_active = False
+      self.lkas_active = False
       
     self.ldws_opt = Params().get_bool('IsLdwsCar')
     self.stock_navi_decel_enabled = Params().get_bool('StockNaviDecelEnabled')
