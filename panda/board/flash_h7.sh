5d2c384faed11de6d4b64910ce2f0688aa95bf2e
Circuit Pro Industries <oricalworks@gmail.com>
2021-08-26 13:53:03 -0400

    read me

--- panda/board/flash_h7.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env sh
-set -e
-
-PANDA_GEN3=1 scons -u
-PYTHONPATH=.. python3 -c "from python import Panda; Panda().flash('obj/panda_h7.bin.signed')"
--- tools/zookeeper/disable.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-
-from tools.zookeeper import Zookeeper
-
-z = Zookeeper()
-z.set_device_power(False)
-
