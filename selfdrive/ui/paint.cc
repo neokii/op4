03c58d744d9b1d4574f9ef0d157adedd4f1afad7
Circuit Pro Industries <oricalworks@gmail.com>
2021-09-10 23:11:33 -0400

    Update paint.cc

--- selfdrive/ui/paint.cc
+++ selfdrive/ui/paint.cc
@@ -1286,7 +1286,6 @@ static void ui_draw_vision_frame(UIState *s) {
   glEnable(GL_SCISSOR_TEST);
   glViewport(s->video_rect.x, s->video_rect.y, s->video_rect.w, s->video_rect.h);
   glScissor(s->viz_rect.x, s->viz_rect.y, s->viz_rect.w, s->viz_rect.h);
-  draw_frame(s);
   glDisable(GL_SCISSOR_TEST);
 
   glViewport(0, 0, s->fb_w, s->fb_h);
