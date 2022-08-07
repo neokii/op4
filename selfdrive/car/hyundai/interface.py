1e14afceb572548e8f033f6e334916e825827c1b
choung hyun lee <67680067+leech2000@users.noreply.github.com>
2022-08-07 19:21:09 +0900

    Update interface.py

--- selfdrive/car/hyundai/interface.py
+++ selfdrive/car/hyundai/interface.py
@@ -181,7 +181,8 @@ class CarInterface(CarInterfaceBase):
     elif candidate in [CAR.KONA_HEV, CAR.KONA_EV]:
       ret.mass = 1395. + STD_CARGO_KG
       ret.wheelbase = 2.6
-      tire_stiffness_factor = 0.7
+      tire_stiffness_factor = 0.385
+      ret.steerRatio = 13.27 * 1.15
       ret.centerToFront = ret.wheelbase * 0.4
     elif candidate in [CAR.IONIQ, CAR.IONIQ_EV_LTD, CAR.IONIQ_EV_2020, CAR.IONIQ_PHEV]:
       ret.mass = 1490. + STD_CARGO_KG
