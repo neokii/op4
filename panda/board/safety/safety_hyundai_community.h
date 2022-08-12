7f3fc8180d27112f7ea8519ebe33fb805c0ace17
neokii <neokii@gmail.com>
2022-08-13 02:51:14 +0900

    merge changes

--- panda/board/safety/safety_hyundai_community.h
+++ panda/board/safety/safety_hyundai_community.h
@@ -200,6 +200,7 @@ static int hyundai_community_tx_hook(CANPacket_t *to_send, bool longitudinal_all
   if (addr == 832) {
     LKAS11_op = 20;
     int desired_torque = ((GET_BYTES_04(to_send) >> 16) & 0x7ff) - 1024;
+    bool steer_req = GET_BIT(to_send, 27U) != 0U;
 
     if (steer_torque_cmd_checks(desired_torque, steer_req, HYUNDAI_STEERING_LIMITS)) {
       tx = 0;
