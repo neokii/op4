f43c2f06e3c6bb91e294bb1b7cbf119c9cc80065
neokii <neokii@gmail.com>
2022-10-06 15:36:27 +0900

    Rocket Launcher Model

--- tinygrad_repo/accel/opencl/ops_opencl.py
+++ tinygrad_repo/accel/opencl/ops_opencl.py
@@ -10,7 +10,8 @@ import numpy as np
 import pyopencl as cl
 
 UNSAFE_FLOAT4 = int(os.getenv("UNSAFE_FLOAT4", 0))
-NATIVE_EXPLOG = int(os.getenv("NATIVE_EXPLOG", 0))
+NATIVE_EXPLOG = int(os.getenv("NATIVE_EXPLOG", 0))  # this is needed as a switch for the tests to pass
+FLOAT16 = int(os.getenv("FLOAT16", 0))
 
 import pathlib
 def load(x):
@@ -20,15 +21,44 @@ def load(x):
 CONV_SRC = load(pathlib.Path(__file__).resolve().parent.parent.parent / 'accel/opencl/conv.cl')
 MATMUL_SRC = load(pathlib.Path(__file__).resolve().parent.parent.parent / 'accel/opencl/matmul.cl')
 
-class ECL(CL):
-  @staticmethod
-  def image(shape):
-    if int(os.getenv("FLOAT16", 0)):
-      # HALF_FLOAT breaks tests
-      fmt = cl.ImageFormat(cl.channel_order.RGBA, cl.channel_type.HALF_FLOAT)
+class CLImage:
+  fmt = cl.ImageFormat(cl.channel_order.RGBA, cl.channel_type.HALF_FLOAT if FLOAT16 else cl.channel_type.FLOAT)
+
+  def __init__(self, shape):
+    self.max_hw = min(CL().cl_ctx.devices[0].image2d_max_width, CL.cl_ctx.devices[0].image2d_max_height)
+    self.shape = shape
+    self.n_tile = int(np.ceil(max(shape) / self.max_hw).item())
+    # if n_tile > 1, we can't fit the image into a CL image at native size,
+    # and need to internally store it as a set of disjoint tiles
+    if self.n_tile * min(shape) > self.max_hw:
+      raise Exception(f"shape {shape} exceeds Metal image limits, even after tiling")
+    if shape[0] >= shape[1]:
+      # wider than it is tall; extra tiles overflow on y
+      self.tile_axis, tiled_width, tiled_height = 1, min(shape[0], self.max_hw), self.n_tile * shape[1]
     else:
-      fmt = cl.ImageFormat(cl.channel_order.RGBA, cl.channel_type.FLOAT)
-    return cl.Image(CL().cl_ctx, cl.mem_flags.READ_WRITE, fmt, shape=shape)
+      # taller than it is wide; extra tiles overflow on x
+      self.tile_axis, tiled_width, tiled_height = 0, self.n_tile * shape[0], min(shape[1], self.max_hw)
+    self.cl = cl.Image(CL.cl_ctx, cl.mem_flags.READ_WRITE, CLImage.fmt, shape=(tiled_width, tiled_height))
+    CL.mem_used += self.cl.row_pitch * self.cl.height
+
+  def pos_to_sample_pos(self, l="l", check_bounds=True):
+    if self.n_tile == 1:
+      # happy path where no indexing ops are needed
+      return l
+    # sad tiled path; need to adjust indices, and manually check bounds for the tiled axis
+    if self.tile_axis == 1:
+      sample_pos = f"((int2)({l}.x % {self.max_hw}, ({l}.x / {self.max_hw}) * {self.shape[1]} + {l}.y))"
+      in_bounds = f"((0 <= {l}.y) && ({l}.y < {self.shape[1]}))"
+    else:
+      sample_pos = f"((int2)(({l}.y / {self.max_hw}) * {self.shape[0]} + {l}.x, {l}.y % {self.max_hw}))"
+      in_bounds = f"((0 <= {l}.x) && ({l}.x < {self.shape[0]}))"
+    if check_bounds:
+      return f"({in_bounds} ? {sample_pos} : (int2)(-1, -1))"
+    return sample_pos
+
+  def __del__(self):
+    if hasattr(self, "cl"):
+      CL.mem_used -= self.cl.row_pitch * self.cl.height
 
 def get_replacements(prg_src:str, opencl_type:List[str]) -> Dict[str, str]:
   middle_code = []
@@ -67,7 +97,7 @@ def get_getters(ewbufs, ret):
     elif buf.is_image() and buf.shape == ret.shape and buf.st.contiguous:
       # use an image here
       ewtypes.append(f"read_only image2d_t {name}_g")
-      getters.append(f"inline float4 get4_{name}(read_only image2d_t x, const sampler_t smp, int2 loc, int gid) {{ return read_imagef(x, smp, loc); }}")
+      getters.append(f"inline float4 get4_{name}(read_only image2d_t x, const sampler_t smp, int2 loc, int gid) {{ return read_imagef(x, smp, {buf._image.pos_to_sample_pos('loc')}); }}")
     elif buf.st.contiguous:
       # use float4
       ewtypes.append(f"__global const float4 *{name}_g")
@@ -77,7 +107,8 @@ def get_getters(ewbufs, ret):
       fakebufs.append(name)
       prt = buf._backing.reshape((-1, 4))
       cc = []
-      for ii in range(prt.shape[0]): cc.append("(float4)(%ff, %ff, %ff, %ff)" % (prt[ii][0], prt[ii][1], prt[ii][2], prt[ii][3]))
+      for ii in range(prt.shape[0]):
+        cc.append("(float4)(%ff, %ff, %ff, %ff)" % (prt[ii][0], prt[ii][1], prt[ii][2], prt[ii][3]))
       getters.append(f"const __constant float4 const_{name}[] = {{"+', '.join(cc)+"};")
       getters.append(f"inline float4 get4_{name}(int gid) {{"+
         "int idx = gid;"+buf.st.expr()+";"+
@@ -104,13 +135,14 @@ class OpenCLBuffer(GPUBuffer):
     UnaryOps.NOOP: "(A)", UnaryOps.NEG: "(-(A))", UnaryOps.RELU: "max(A, (float)0.)", UnaryOps.SIGN: "sign(A)",
     UnaryOps.EXP: "native_exp(A)" if NATIVE_EXPLOG else "exp(A)",
     UnaryOps.LOG: "native_log(A)" if NATIVE_EXPLOG else "log(A)",
-    UnaryOps.RECIPROCAL: "native_recip(A)" if NATIVE_EXPLOG else "(1.0/A)",
+    UnaryOps.RECIPROCAL: "native_recip(A)" if NATIVE_EXPLOG else "((float)1.0/A)",
     BinaryOps.ADD: "(A+B)", BinaryOps.SUB: "(A-B)", BinaryOps.MUL: "(A*B)", BinaryOps.DIV: "(A/B)", BinaryOps.POW: "pow(A,B)", BinaryOps.CMPEQ: "(A==B)",
     ReduceOps.SUM: "(acc + A)", ReduceOps.MAX: "max(A, acc)"
   }
   def __init__(self, shape, hostbuf:Optional[OpenCLBuffer]=None, backing:Optional[np.ndarray]=None):
     self._image = hostbuf._image if hostbuf is not None else None
     super().__init__(shape, hostbuf, backing)
+    assert not (self._image and self._buf)
 
   @staticmethod
   def fromCPU(x): return OpenCLBuffer(x.shape, backing=x.view(np.ndarray).astype(np.float32).ravel())
@@ -132,18 +164,19 @@ class OpenCLBuffer(GPUBuffer):
           CL.enqueue_copy(self._buf.cl, self._backing, is_blocking=False)
           #self._backing = None
         #print(f"converting {self.shape} back to buffer, image shape is {self._image.shape}")
-        CLProgram("from_image", """
+        CLProgram("from_image", f"""
           __kernel void from_image(
               __global float4 *out,
-              read_only image2d_t in) {
+              read_only image2d_t in) {{
             const sampler_t smp = CLK_NORMALIZED_COORDS_FALSE | CLK_ADDRESS_CLAMP | CLK_FILTER_NEAREST;
             int2 l;
             l.y = get_global_id(1);
             l.x = get_global_id(0);
-            int W = get_image_width(in);
-            out[l.y*W + l.x] = read_imagef(in, smp, l);
-          }
-        """)(self._image.shape, None, self._buf.cl, self._image)
+            int2 l_smp = {self._image.pos_to_sample_pos('l')};
+            int W = {str(self._image.shape[0])};
+            out[l.y*W + l.x] = read_imagef(in, smp, l_smp);
+          }}
+        """)(self._image.shape, None, self._buf.cl, self._image.cl)
         self._image = None
     return self._buf.cl
   
@@ -153,23 +186,24 @@ class OpenCLBuffer(GPUBuffer):
   def image(self):
     if self._image is None:
       assert len(self.shape) == 3 and self.shape[2] == 4, f"bad shape for image {self.shape}"
-      self._image = ECL.image(shape=(self.shape[1], self.shape[0]))
+      self._image = CLImage(shape=(self.shape[1], self.shape[0]))
       if self._buf is not None:
-        assert prod(self.shape) == prod(self._image.shape)*4
+        assert prod(self.shape) <= prod(self._image.cl.shape)*4
         #print(f"converting {self.shape} to image with shape {self._image.shape}")
-        CLProgram("to_image", """
+        CLProgram("to_image", f"""
           __kernel void to_image(
               write_only image2d_t out,
-              __global const float4 *in) {
+              __global const float4 *in) {{
             int2 l;
             l.y = get_global_id(1);
             l.x = get_global_id(0);
-            int W = get_image_width(out);
-            write_imagef(out, l, in[l.y*W + l.x]);
-          }
-        """)(self._image.shape, None, self._image, self._buf.cl)
+            int2 l_out = {self._image.pos_to_sample_pos('l', check_bounds=False)};
+            int W = {str(self._image.shape[0])};
+            write_imagef(out, l_out, in[l.y*W + l.x]);
+          }}
+        """)(self._image.shape, None, self._image.cl, self._buf.cl)
       self._buf = None
-    return self._image
+    return self._image.cl
 
   SUPPORTS_PADDING = True
   def processing_op(x, op:ProcessingOps, w:GPUBuffer, C:ConvArgs):
@@ -182,17 +216,17 @@ class OpenCLBuffer(GPUBuffer):
       return f"""inline float get_{name}(const sampler_t smp, read_only image2d_t x, int gid) {{
         int valid = 1; int idx = gid; {x.st.expr().replace('//', '/')};
         int2 l;
-        int W = get_image_width(x);
+        int W = {str(x._image.shape[0])};
         l.y = idx / (W*4);
         l.x = (idx/4) % W;
         int idx4 = idx % 4;
-        float4 dat = read_imagef(x, smp, l);
+        int2 l_smp = {x._image.pos_to_sample_pos('l')};
+        float4 dat = read_imagef(x, smp, l_smp);
         return valid ? (idx4 == 0 ? dat.x : (idx4 == 1 ? dat.y : (idx4 == 2 ? dat.z : dat.w))) : 0.0;
       }}""", f"read_only image2d_t {name}_g", f"get_{name}(smp, {name}_g, idx);"
     #ewtypes.append(f"read_only image2d_t {name}_g")
     return super().contiguous_view_constant_fold(name)
 
-  seen = set()
   def _processing_op(ret, bufs: List[Tuple[str, OpenCLBuffer]]=[], code:str="acc", C=None, op=ReduceOps.SUM, reduce_shape=None, earlybufs:Set[str]=set(), earlycode:str="acc"):
     if C is None or earlycode != "acc":
       # TODO: handle an opencl conv without the conv part
@@ -202,10 +236,6 @@ class OpenCLBuffer(GPUBuffer):
     x = [x for x in bufs if x[0] == "input"][0][1]
     w = [x for x in bufs if x[0] == "weight"][0][1]
     ewbufs = [x for x in bufs if x[0] not in ["input", "weight"]]
-
-    if (x,w) in OpenCLBuffer.seen:
-      print("WARNING: recomputing CONV with", x, w)
-    OpenCLBuffer.seen.add((x,w))
 
     # remove fakebufs
     fakebufs, ewtypes, getters = get_getters(ewbufs, ret)
@@ -219,14 +249,24 @@ class OpenCLBuffer(GPUBuffer):
 
     replacements = get_replacements(elementwise_prefix, ewtypes)
 
+    (x.image, w.image, ret.image)
+    # fix sampling
+    replacements["INPUT_LOCATION"] = x._image.pos_to_sample_pos("inputLocation")
+    replacements["WEIGHT_LOCATION"] = w._image.pos_to_sample_pos("weightLocation")
+    replacements["OUTPUT_LOCATION"] = ret._image.pos_to_sample_pos("outputLocation", check_bounds=False)
+    # fix widths
+    replacements["get_image_width(output)"] = f"({ret._image.shape[0]})"
+
     x, w = x.contiguous_op(), w.contiguous_op()
     options = []
     if C.bs > 1:
       options.append("-DBATCH")
       assert C.py == 0, "batched conv doesn't work with y-padding"
-    if C.sx == 1 and C.sy == 1 and C.dx == 1 and C.dy == 1 and C.cin == 1: options.append("-DDEPTHWISE_UNSTRIDED")
-    elif C.cin == 1: options.append("-DDEPTHWISE")
-    if int(os.getenv("MATMUL", 0)) and C.groups == 1 and C.H == 1 and C.W == 1 and C.iy == 1 and C.ix == 1 and C.oy == 1 and C.ox == 1 and C.sx == 1 and C.sy == 1 and C.dx == 1 and C.dy == 1:
+    if C.sx == 1 and C.sy == 1 and C.dx == 1 and C.dy == 1 and C.cin == 1:
+      options.append("-DDEPTHWISE_UNSTRIDED")
+    elif C.cin == 1:
+      options.append("-DDEPTHWISE")
+    if C.groups == 1 and C.H == 1 and C.W == 1 and C.iy == 1 and C.ix == 1 and C.oy == 1 and C.ox == 1 and C.sx == 1 and C.sy == 1 and C.dx == 1 and C.dy == 1 and C.bs == 1:
       options.append("-DMATMUL")
       # NOTE: this is not actually a matmul, it's a vector * matrix
 
@@ -259,7 +299,8 @@ class OpenCLBuffer(GPUBuffer):
       return ret
 
     # this option is unused
-    if C.H == 1 and C.W == 1: options.append("-DONLY_1X1_CONV")
+    if C.H == 1 and C.W == 1:
+      options.append("-DONLY_1X1_CONV")
 
     assert C.cout%4 == 0
     conv_src = CONV_SRC
--- tinygrad_repo/tinygrad/tensor.py
+++ tinygrad_repo/tinygrad/tensor.py
@@ -2,7 +2,7 @@
 from __future__ import annotations
 import inspect, functools, importlib, itertools
 import numpy as np
-from tinygrad.helpers import prod
+from tinygrad.helpers import prod, argfix
 from typing import List, Tuple, Callable, Optional
 from tinygrad.ops import Device
 
@@ -13,7 +13,7 @@ from tinygrad.ops import LazyBuffer
 class Tensor:
   training, no_grad = False, False
 
-  def __init__(self, data, device=Device.DEFAULT, requires_grad=True):
+  def __init__(self, data, device=Device.DEFAULT, requires_grad=None):
     if isinstance(data, list):
       data = np.array(data, dtype=np.float32)
     elif isinstance(data, LazyBuffer) and data.device != device:
@@ -21,14 +21,20 @@ class Tensor:
       data = data.realize().toCPU()
 
     if isinstance(data, np.ndarray):
-      if data.shape == tuple(): data = data.reshape((1,))
+      if data.shape == tuple():
+        data = data.reshape((1,))
       self.lazydata = LazyBuffer.fromCPU(data.astype(np.float32), device)
-    elif isinstance(data, LazyBuffer): self.lazydata = data
-    else: raise Exception(f"can't create Tensor from {data}")
+    elif isinstance(data, LazyBuffer):
+      self.lazydata = data
+    else:
+      raise Exception(f"can't create Tensor from {data}")
 
     # tensors have gradients, buffers do not
     self.grad : Optional[Tensor] = None
-    self.requires_grad = requires_grad
+
+    # NOTE: this can be in three states. False and None: no gradient, True: gradient
+    # None (the default) will be updated to True if it's put in an optimizer
+    self.requires_grad : Optional[bool] = requires_grad
 
     # internal variables used for autograd graph construction
     self._ctx : Optional[Function] = None
@@ -53,14 +59,15 @@ class Tensor:
     return self
 
   def assign(self, x):
-    if not isinstance(x, Tensor): x = Tensor(x)
+    if not isinstance(x, Tensor):
+      x = Tensor(x)
     assert self.shape == x.shape
     self.lazydata = x.lazydata
     return x
 
   def detach(self): return Tensor(self.lazydata, device=self.device, requires_grad=False)
   def numpy(self): return np.array(self.lazydata.toCPU())
-  
+
   # TODO: this keeps the legacy behavior working, remove it after refactor
   @property
   def data(self): return self.numpy()
@@ -69,11 +76,13 @@ class Tensor:
   def to_(self, device:str):
     assert self.lazydata.realized is None
     self.lazydata.device = device
-    if self.grad: self.grad.lazydata.device = device
+    if self.grad:
+      self.grad.lazydata.device = device
 
   def to(self, device:str):
     ret = Tensor(self.lazydata, device)
-    if self.grad: ret.grad = self.grad.to(device)
+    if self.grad:
+      ret.grad = self.grad.to(device)
     return ret
 
   # ***** creation helper functions *****
@@ -87,16 +96,29 @@ class Tensor:
   def ones(cls, *shape, **kwargs): return cls(np.ones(shape, dtype=np.float32), **kwargs)
 
   @classmethod
-  def randn(cls, *shape, **kwargs): return cls(np.random.randn(*shape).astype(np.float32), **kwargs)
-  
-  @classmethod
-  def arange(cls, stop, start=0, **kwargs): return cls(np.arange(start=start, stop=stop).astype(np.float32), **kwargs)
+  def empty(cls, *shape, **kwargs): return cls(np.empty(shape, dtype=np.float32), **kwargs)
 
   @classmethod
-  def uniform(cls, *shape, **kwargs): return cls((np.random.uniform(-1., 1., size=shape)/np.sqrt(prod(shape))).astype(np.float32), **kwargs)
+  def randn(cls, *shape, **kwargs): return cls(np.random.default_rng().standard_normal(size=shape, dtype=np.float32), **kwargs)
 
   @classmethod
-  def eye(cls, dim, **kwargs): return cls(np.eye(dim).astype(np.float32), **kwargs)
+  def arange(cls, stop, start=0, **kwargs): return cls(np.arange(start=start, stop=stop, dtype=np.float32), **kwargs)
+
+  # TODO: uniform should be a late binding thing
+  # Return random number between -1 and 1
+  # NOTE: this behavior changed from depending on the shape to not
+  @classmethod
+  def uniform(cls, *shape, **kwargs): return cls((np.random.default_rng().random(size=shape, dtype=np.float32) * 2 - 1), **kwargs)
+
+  @classmethod
+  def scaled_uniform(cls, *shape, **kwargs): return cls((np.random.default_rng().random(size=shape, dtype=np.float32) * 2 - 1) * (prod(shape)**-0.5), **kwargs)
+
+  @classmethod
+  # https://www.tensorflow.org/api_docs/python/tf/keras/initializers/GlorotUniform
+  def glorot_uniform(cls, *shape, **kwargs): return cls((np.random.default_rng().random(size=shape, dtype=np.float32) * 2 - 1) * ((6/(shape[0]+prod(shape[1:])))**0.5), **kwargs)
+
+  @classmethod
+  def eye(cls, dim, **kwargs): return cls(np.eye(dim, dtype=np.float32), **kwargs)
 
   # ***** toposort and backward pass *****
 
@@ -117,7 +139,8 @@ class Tensor:
     self.grad = Tensor.ones(*self.shape, device=self.device, requires_grad=False)
 
     for t0 in reversed(self.deepwalk()):
-      if not any(x.requires_grad for x in t0._ctx.parents): continue
+      if not any(x.requires_grad for x in t0._ctx.parents):
+        continue
       assert (t0.grad is not None)
       grads = t0._ctx.backward(t0.grad.lazydata)
       grads = [Tensor(g, device=self.device, requires_grad=False) if g is not None else None
@@ -129,33 +152,45 @@ class Tensor:
       del t0._ctx
 
   # ***** non first class ops (hlops) *****
-  
+
   def __getitem__(self, val):
-    arg = []
-    for i, s in enumerate(val if isinstance(val, (list, tuple)) else [val]) if val is not None else []:
-      if isinstance(s, int): s = slice(s, s+1, None)
-      arg.append((s.start if s.start is not None else 0,
-        (s.stop if s.stop >=0 else self.shape[i]+s.stop) if s.stop is not None else self.shape[i]))
+    arg, new_shape = [], []
+    for i, rs in enumerate(val if isinstance(val, (list, tuple)) else [val]) if val is not None else []:
+      s = slice(rs, rs+1, None) if isinstance(rs, int) else rs
+      arg.append((s.start if s.start is not None else 0, (s.stop if s.stop>=0 else self.shape[i]+s.stop) if s.stop is not None else self.shape[i]))
       assert s.step is None or s.step == 1
-    return self.slice(arg = arg + [(0,self.shape[i]) for i in range(len(arg), len(self.shape))])
+      if not isinstance(rs, int):  # don't include in shape if it's an int
+        new_shape.append(arg[-1][1] - arg[-1][0])
+    new_shape += [self.shape[i] for i in range(len(arg), len(self.shape))]
+    return self.slice(arg = arg + [(0,self.shape[i]) for i in range(len(arg), len(self.shape))]).reshape(new_shape if len(new_shape) else (1,))
 
   def cat(self, *args, dim=0):
     dim = (dim + len(self.shape)) if dim < 0 else dim
-    for y in args: assert len(y.shape) == len(self.shape) and all(y.shape[i] == s for i,s in enumerate(self.shape) if i != dim)
+    for y in args:
+      assert len(y.shape) == len(self.shape) and all(y.shape[i] == s for i,s in enumerate(self.shape) if i != dim)
     args = [self] + list(args)
     shape_cumsum = [0, *itertools.accumulate(y.shape[dim] for y in args)]
     slc = [[(0, s) for s in self.shape] for _ in args]
-    for s,k in zip(slc, shape_cumsum): s[dim] = (-k, shape_cumsum[-1]-k)
-    slices = [arg.slice(arg=s) for arg,s in zip(args, slc)]
-    return functools.reduce(Tensor.__iadd__, slices)
+    for s,k in zip(slc, shape_cumsum):
+      s[dim] = (-k, shape_cumsum[-1]-k)
+    return functools.reduce(Tensor.__iadd__, [arg.slice(arg=s) for arg,s in zip(args, slc)])
+
+  # TODO: make this nicer with syntactic sugar in slice
+  def chunk(self, num, dim):
+    slice_params = [[(0, s) for s in self.shape] for _ in range(num)]
+    for i,k in enumerate(range(0, self.shape[dim], self.shape[dim]//num)):
+      slice_params[i][dim] = (k, min(self.shape[dim], k+self.shape[dim]//num))
+    return [self.slice(arg=p) for p in slice_params]
 
   def matmul(self:Tensor, w:Tensor):
     # NOTE: we use a 1x1 conv2d to do the matmul. mxk @ kxn = (1,k,m,1).conv2d(n,k,1,1)
     bs, groups = prod(self.shape[0:-2]), prod(w.shape[0:-2])
     cin, cout = w.shape[-2], w.shape[-1]
     out_shape_t = tuple(list(self.shape[0:-2])+[cout,-1])
-    if len(self.shape) > 1: order = tuple(list(range(len(self.shape)-2))+[len(self.shape)-1, len(self.shape)-2])
-    else: order, out_shape_t = (0,), (cout, )
+    if len(self.shape) > 1:
+      order = tuple(list(range(len(self.shape)-2))+[len(self.shape)-1, len(self.shape)-2])
+    else:
+      order, out_shape_t = (0,), (cout, )
     worder = tuple(list(range(len(w.shape)-2))+[len(w.shape)-1, len(w.shape)-2])
 
     # NOTE: with NHWC we can remove the transposes
@@ -170,19 +205,23 @@ class Tensor:
 
   # (padding_left, padding_right, padding_top, padding_bottom)
   def pad2d(self, padding:Tuple[int, ...]): return self[:, :, -padding[2]:self.shape[2]+padding[3], -padding[0]:self.shape[3]+padding[1]]
+  # TODO: this is totally not transpose
   def transpose(self, order=(1,0)): return self.permute(order=order)
   def flatten(self, start_dim=0): return self.reshape(shape=tuple(list(self.shape[0:start_dim]) + [-1]))
 
   def _reduce(self, fxn, axis=None, keepdim=False):
-    if axis is None: axis = range(len(self.shape))
-    if isinstance(axis, int): axis = [axis]
+    if axis is None:
+      axis = range(len(self.shape))
+    if isinstance(axis, int):
+      axis = [axis]
     axis = tuple([x if x >= 0 else x+len(self.shape) for x in axis])
     shape = [self.shape[i] for i in range(len(self.shape)) if i not in axis]
-    ret = fxn(axis=axis)
+    ret = fxn(self, axis=axis)
     return ret if keepdim else ret.reshape(shape=[1] if shape == [] else shape)
 
-  def sum(self, axis=None, keepdim=False): return self._reduce(self._sum, axis, keepdim)
-  def max(self, axis=None, keepdim=False): return self._reduce(self._max, axis, keepdim)
+  def sum(self, axis=None, keepdim=False): return self._reduce(Tensor._sum, axis, keepdim)
+  def max(self, axis=None, keepdim=False): return self._reduce(Tensor._max, axis, keepdim)
+  def min(self, axis=None, keepdim=False): return -((-self).max(axis=axis, keepdim=keepdim))
 
   def mean(self, axis=None, keepdim=False):
     out = self.sum(axis=axis, keepdim=keepdim)
@@ -202,7 +241,8 @@ class Tensor:
     return m - ss.log()
 
   def dropout(self, p=0.5):
-    if not Tensor.training: return self
+    if not Tensor.training:
+      return self
     _mask = np.asarray(np.random.binomial(1, 1.0-p, size=self.shape), dtype=self.dtype)
     return self * Tensor(_mask, requires_grad=False, device=self.device) * (1/(1.0 - p))
 
@@ -222,6 +262,7 @@ class Tensor:
 
   def __neg__(self): return 0.0-self
   def sqrt(self): return self.pow(0.5)
+  def square(self): return self*self
   def clip(self, min_, max_): return ((self-min_).relu()+min_) - (self-max_).relu()
   def abs(self): return self.relu() + (-self).relu()
   def sign(self): return self / (self.abs() + 1e-10)
@@ -229,13 +270,14 @@ class Tensor:
   # ***** activation functions (unary) *****
 
   def sigmoid(self): return (1.0 + (-self).exp()).reciprocal()
-  # TODO: implement generic constant folding
   def elu(self, alpha=1.0): return self.relu() - alpha*(1-self.exp()).relu()
   def swish(self): return self * self.sigmoid()
+  silu = swish   # The SiLU function is also known as the swish function.
   def relu6(self): return self.relu() - (self-6).relu()
   def hardswish(self): return self * (self+3).relu6() * (1/6)
   def tanh(self): return 2.0 * ((2.0 * self).sigmoid()) - 1.0
   def gelu(self): return 0.5 * self * (1 + (self * 0.7978845608 * (1 + 0.044715 * self * self)).tanh())
+  def quick_gelu(self): return self * (self * 1.702).sigmoid()
   def leakyrelu(self, neg_slope=0.01): return self.relu() - (-neg_slope*self).relu()
   def mish(self): return self * self.softplus().tanh()
   def softplus(self, limit=20, beta=1): return (1/beta) * (1 + (self*beta).exp()).log()
@@ -246,7 +288,7 @@ class Tensor:
   def broadcasted(fxn, x, y):
     tt = [arg for arg in [x,y] if isinstance(arg, Tensor)][0]  # this is the prototype tensor
     x,y = [Tensor([t], device=tt.device, requires_grad=False) if not isinstance(t, Tensor) else t for t in [x,y]]
-    x,y = [t.reshape(list(t.shape) + [1]*(max(len(x.shape), len(y.shape))-len(t.shape))) for t in [x,y]]
+    x,y = [t.reshape([1]*(max(len(x.shape), len(y.shape))-len(t.shape)) + list(t.shape)) for t in [x,y]]
     shape_ret = tuple(max(sx, sy) for sx,sy in zip(x.shape, y.shape))
     return fxn(x.expand(shape_ret), y.expand(shape_ret))
 
@@ -255,32 +297,31 @@ class Tensor:
   def sub(self, x): return Tensor.broadcasted(Tensor._sub, self, x)
   def mul(self, x): return Tensor.broadcasted(Tensor._mul, self, x)
   def pow(self, x): return Tensor.broadcasted(Tensor._pow, self, x)
-  def div(self, y): return self * y.reciprocal()
-  __truediv__ = div
+  def div(self, y): return self * (y.reciprocal() if isinstance(y, Tensor) else (1/y))
 
   # ***** functional nn ops *****
 
-  # TODO: fix the kwargs problem, then remove these
-  # NOTE: perhaps don't, since they create NOOPs if the shape already matches
-  def reshape(self, shape): return self._reshape(shape=shape) if tuple(self.shape) != tuple(shape) else self
-  def expand(self, shape): return self._expand(shape=shape) if tuple(self.shape) != tuple(shape) else self
+  # TODO: fix the kwargs problem, then remove these (or not, since they now fix tuples)
+  def reshape(self, shape, *args): return self._reshape(shape=argfix(shape, *args))
+  def expand(self, shape, *args): return self._expand(shape=argfix(shape, *args))
+  def permute(self, order, *args): return self._permute(order=argfix(order, *args))
 
-  def linear(self, weight:Tensor, bias:Tensor):
-    shp = [1] * (len(self.shape)-1) + [-1]
-    return (self.mul(weight.reshape(shape=shp)) if len(weight.shape) == 1 else self.dot(weight)).add(bias.reshape(shape=shp))
+  def linear(self, weight:Tensor, bias:Optional[Tensor]=None):
+    x = self.mul(weight) if len(weight.shape) == 1 else self.dot(weight)
+    return x.add(bias) if bias is not None else x
 
   def sequential(self, ll:List[Callable[[Tensor], Tensor]]): return functools.reduce(lambda x,f: f(x), ll, self)
 
-  def layernorm(self, eps=1e-5):
-    y = (self - self.mean(axis=-1, keepdim=True))
-    return y.div((y*y).mean(axis=-1, keepdim=True).add(eps).sqrt())
+  def layernorm(self, axis=-1, eps=1e-5):
+    y = (self - self.mean(axis=axis, keepdim=True))
+    return y.div((y*y).mean(axis=axis, keepdim=True).add(eps).sqrt())
 
 # An instantiation of the Function is the Context
 class Function:
   def __init__(self, device:str, *tensors:Tensor):
     self.device, self.parents = device, tensors
     self.needs_input_grad = [t.requires_grad for t in self.parents]
-    self.requires_grad = any(self.needs_input_grad)
+    self.requires_grad = True if any(self.needs_input_grad) else (None if any([x is None for x in self.needs_input_grad]) else False)
     self.saved_tensors : List[Tensor] = []
 
   def forward(self, *args, **kwargs): raise NotImplementedError(f"forward not implemented for {type(self)}")
@@ -293,7 +334,8 @@ class Function:
   def apply(cls, *x:Tensor, **kwargs):
     ctx = cls(x[0].device, *x)
     ret = Tensor(ctx.forward(*[t.lazydata for t in x], **kwargs), device=ctx.device, requires_grad=ctx.requires_grad)
-    if ctx.requires_grad and not Tensor.no_grad: ret._ctx = ctx    # used by autograd engine
+    if ctx.requires_grad and not Tensor.no_grad:
+      ret._ctx = ctx    # used by autograd engine
     return ret
 
 # register functions to move between devices
@@ -305,12 +347,13 @@ for device in [device for device in Device.__dict__.keys() if device[0] != "_"]:
 def register(name:str, fxn:Function):
   setattr(Tensor, "_"+name if (getattr(Tensor, name, None) is not None) else name, functools.partialmethod(fxn.apply))
 for name, cls in inspect.getmembers(importlib.import_module('tinygrad.mlops'), inspect.isclass):
-  if name[0] != "_" and name != "Function" and not name.endswith("Ops"): register(name.lower(), cls)
+  if name[0] != "_" and name != "Function" and not name.endswith("Ops"):
+    register(name.lower(), cls)
 
 # register the operators
-# TODO: add div
 def register_op(name, fxn):
   setattr(Tensor, f"__{name}__", fxn)
   setattr(Tensor, f"__i{name}__", lambda self,x: self.assign(fxn(self,x)))
   setattr(Tensor, f"__r{name}__", lambda self,x: fxn(x,self))
-for name in ['add', 'sub', 'mul', 'pow', 'matmul']: register_op(name, getattr(Tensor, name))
+for name in ['add', 'sub', 'mul', 'pow', 'matmul', 'truediv']:
+  register_op(name, getattr(Tensor, name if name != 'truediv' else 'div'))
